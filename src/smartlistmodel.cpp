a19f824c8cbf381082fb63678e404a1572e84ff1
ababi <albert.babi@savoirfairelinux.com>
2020-08-24 17:46:30 +0200

    mainview: get call state from API's enumeration
    In order to make it work with the translations, call status is no longer passed as a string but as an integer (from enum lrc::api::call:Status).
    
    Gitlab: #10
    Change-Id: If8d8b7093fbf82e9b7732e6991eec647ad0d50b7

--- src/smartlistmodel.cpp
+++ src/smartlistmodel.cpp
@@ -161,7 +161,7 @@ SmartListModel::roleNames() const
     roles[InCall] = "InCall";
     roles[IsAudioOnly] = "IsAudioOnly";
     roles[CallStackViewShouldShow] = "CallStackViewShouldShow";
-    roles[CallStateStr] = "CallStateStr";
+    roles[CallState] = "CallState";
     roles[SectionName] = "SectionName";
     roles[AccountId] = "AccountId";
     roles[Draft] = "Draft";
@@ -341,14 +341,13 @@ SmartListModel::getConversationItemData(const conversation::Info &item,
         }
         return QVariant(false);
     }
-    case Role::CallStateStr: {
+    case Role::CallState: {
         auto* convModel = LRCInstance::getCurrentConversationModel();
         const auto convInfo = convModel->getConversationForUID(item.uid);
         if (!convInfo.uid.isEmpty()) {
             auto* call = LRCInstance::getCallInfoForConversation(convInfo);
             if (call) {
-                auto statusString = call::to_string(call->status);
-                return QVariant(statusString);
+                return QVariant(static_cast<int>(call->status));
             }
         }
         return QVariant();
