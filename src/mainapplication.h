8645676a9ef2ccf73f45773b3af02b1bed97dffe
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2020-09-17 16:08:52 -0400

    (WIP)misc: implement update system
    - re-introduce a genericized NetworkManager
    - isolate update logic into a qml accessible class derived from
      NetworkManager
    - fix QtWebEngineProcess missing when re-installing over existing
      version
    - provide a command line option to override the base url
      for testing local and remote updates
    - clean-up manual update-check UI
    
    Gitlab: #101
    Change-Id: I9c8d2badae59ec31cab12d38b8470edf2bcad401

--- src/mainapplication.h
+++ src/mainapplication.h
@@ -41,14 +41,15 @@ public:
 
 private:
     void loadTranslations();
-    void initLrc();
+    void initLrc(const QString& downloadUrl);
     void initConnectivityMonitor();
-    void parseArguments(bool& startMinimized);
+    const QVariantMap parseArguments();
     void setApplicationFont();
     void initQmlEngine();
     void initSettings();
     void initSystray();
     void cleanup();
+    void connectForceWindowToTop();
 
 private:
 #ifdef Q_OS_WIN
