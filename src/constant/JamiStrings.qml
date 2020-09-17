9e0619a0ddcdde5f9d763f376e2e041838750b67
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2020-09-17 16:08:52 -0400

    misc: implement update system
    Change-Id: I9c8d2badae59ec31cab12d38b8470edf2bcad401

--- src/constant/JamiStrings.qml
+++ src/constant/JamiStrings.qml
@@ -268,9 +268,19 @@ Item {
     property string tipChooseDownloadFolder: qsTr("Choose download directory")
     property string recordCall: qsTr("Record call")
 
-    // UpdateSettings
+    // Updates
     property string betaInstall: qsTr("Install beta version")
+    property string checkForUpdates: qsTr("Check for updates now")
     property string enableAutoUpdates: qsTr("Enable/Disable automatic updates")
+    property string tipAutoUpdate: qsTr("toggle automatic updates")
+    property string updatesTitle: qsTr("Updates")
+    property string updateDialogTitle: qsTr("Update")
+    property string updateFound: qsTr("A new version of Jami was found. Would you like to update now?")
+    property string updateNotFound: qsTr("No new version found")
+    property string updateCheckError: qsTr("An error occured when checking for a new version")
+    property string updateDownloadNetworkError: qsTr("Installer download failed due to a network error")
+    property string updateDownloadCanceled: qsTr("Installer download canceled")
+    property string updateDownloading: "Downloading"
 
     // Recording Settings
     property string tipRecordFolder: qsTr("Select a record directory")
@@ -365,4 +375,8 @@ Item {
 
     // Update settings
     property string update: qsTr("Automatically check for updates")
+
+    // Generic dialog options
+    property string optionOk: qsTr("Ok")
+    property string optionCancel: qsTr("Cancel")
 }
---
+++ src/updatemanager.cpp
@@ -0,0 +1,128 @@
+/*!
+ * Copyright (C) 2020 by Savoir-faire Linux
+ * Author: Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "updatemanager.h"
+
+#include "utils.h"
+#include "version.h"
+
+#ifdef BETA
+static constexpr bool isBeta = true;
+#else
+static constexpr bool isBeta = false;
+#endif
+
+UpdateManager::UpdateManager(QObject* parent)
+    : NetWorkManager(parent)
+    , baseUrl_("https://dl.jami.net/windows")
+    , tempPath_(Utils::WinGetEnv("TEMP"))
+{}
+
+void
+UpdateManager::checkForUpdates(bool quiet)
+{
+    disconnect();
+    connect(this, &NetWorkManager::errorOccured, this, &UpdateManager::updateCheckErrorOccurred);
+
+    cleanUpdateFiles();
+    QUrl versionUrl {isBeta ? QUrl::fromEncoded(baseUrl_ + "/beta/version")
+                            : QUrl::fromEncoded(baseUrl_ + "/version")};
+
+    get(versionUrl, [this, quiet](const QString& latest) {
+        if (latest.isEmpty()) {
+            qWarning() << "Error downloading version file";
+            if (!quiet)
+                emit updateCheckReplyReceived(false);
+            return;
+        }
+        auto currentVersion = QString(VERSION_STRING).toULongLong();
+        if (latest.toULongLong() < currentVersion) {
+            emit updateCheckReplyReceived(true, true);
+        } else {
+            qDebug() << "No new version found";
+            if (!quiet)
+                emit updateCheckReplyReceived(true, false);
+        }
+    });
+}
+
+void
+UpdateManager::applyUpdates(bool beta)
+{
+    disconnect();
+    connect(this, &NetWorkManager::errorOccured, this, &UpdateManager::updateDownloadErrorOccurred);
+    connect(this, &NetWorkManager::statusChanged, [this](GetStatus status) {
+        switch (status) {
+        case GetStatus::STARTED:
+            connect(this,
+                    &NetWorkManager::downloadProgressChanged,
+                    this,
+                    &UpdateManager::updateDownloadProgressChanged);
+            emit updateDownloadStarted();
+            break;
+        case GetStatus::FINISHED:
+            emit updateDownloadFinished();
+            break;
+        default:
+            break;
+        }
+    });
+
+    QUrl downloadUrl {(beta || isBeta) ? QUrl::fromEncoded(baseUrl_ + "/beta/jami.beta.x64.msi")
+                                       : QUrl::fromEncoded(baseUrl_ + "/jami.release.x64.msi")};
+
+    get(
+        downloadUrl,
+        [this, downloadUrl](const QString&) {
+            auto args = QString(" /passive /norestart WIXNONUILAUNCH=1");
+            auto cmdStartInstaller = "powershell " + tempPath_ + "\\" + downloadUrl.fileName()
+                                     + " /L*V " + tempPath_ + "\\jami_x64_install.log" + args;
+            emit appCloseRequested();
+            // LRCInstance::instance().reset();
+
+            // QProcess::startDetached(cmdStartInstaller);
+        },
+        tempPath_);
+}
+
+void
+UpdateManager::cancelUpdate()
+{
+    cancelRequest();
+}
+
+void
+UpdateManager::cleanUpdateFiles()
+{
+    /*
+     * Delete all logs and msi in the %TEMP% directory before launching.
+     */
+    QString dir = QString(Utils::WinGetEnv("TEMP"));
+    QDir log_dir(dir, {"jami*.log"});
+    for (const QString& filename : log_dir.entryList()) {
+        log_dir.remove(filename);
+    }
+    QDir msi_dir(dir, {"jami*.msi"});
+    for (const QString& filename : msi_dir.entryList()) {
+        msi_dir.remove(filename);
+    }
+    QDir version_dir(dir, {"version"});
+    for (const QString& filename : version_dir.entryList()) {
+        version_dir.remove(filename);
+    }
+}
