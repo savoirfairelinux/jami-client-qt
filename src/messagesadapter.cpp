777bdf51baf5bbd7fdb73db3c6ea418ee2daed49
Ming Rui Zhang <mingrui.zhang@savoirfairelinux.com>
2020-10-01 16:02:18 -0400

    wizardview: adaption for added best name and id logic in lrc
    Note: if the Displayname/alias is the same as the username,
          it should be the only line shown
    
    Gitlab: #129
    Change-Id: I449e7b8b4014ef99a44a2b4c5e6149ba0148d96e

--- src/messagesadapter.cpp
+++ src/messagesadapter.cpp
@@ -448,9 +448,9 @@ MessagesAdapter::setConversationProfileData(const lrc::api::conversation::Info& 
     }
     try {
         auto& contact = accInfo->contactModel->getContact(contactUri);
-        auto bestName = Utils::bestNameForConversation(convInfo, *convModel);
+        auto bestName = accInfo->contactModel->bestNameForContact(contactUri);
         setInvitation(contact.profileInfo.type == lrc::api::profile::Type::PENDING
-                      || contact.profileInfo.type == lrc::api::profile::Type::TEMPORARY,
+                          || contact.profileInfo.type == lrc::api::profile::Type::TEMPORARY,
                       bestName,
                       contactUri);
 
