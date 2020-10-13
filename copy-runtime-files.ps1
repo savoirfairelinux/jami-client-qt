30a66e1143da6b6dd6d916ddd9acf2f3ee2be6ed
Ming Rui Zhang <mingrui.zhang@savoirfairelinux.com>
2020-10-13 13:49:31 -0400

    misc: Qt 5.14 adaptation for windows
    Change-Id: Ic8aad234c8806bd38a1b1633957503b09a01c137

--- copy-runtime-files.ps1
+++ copy-runtime-files.ps1
@@ -18,14 +18,15 @@ If (test-path $stampFile) {
 }
 
 # default values
-$qtver = If ($qtver) { $qtver } Else { "5.15.0" }
+$qtver = If ($qtver) { $qtver } Else { "5.14.2" }
+$qtMinorVer = $qtver.split('.')[1]
 $mode = If ($mode) { $mode } Else { "Release" }
 
 if (!$outDir) { $outDir = $clientDir + "\x64\" + $mode }
 If (!(test-path $outDir)) { New-Item -ItemType directory -Path $outDir -Force }
 
 $qtverSplit1, $qtverSplit2 , $qtverSplit3 = $qtver.Split('.')
-$qtMsvcDir = "msvc2019_64"
+$qtMsvcDir = If (([int]$qtMinorVer) -le 14) {"msvc2017_64"} Else {"msvc2019_64"}
 
 $QtDir = "C:\Qt\$qtver\$qtMsvcDir"
 
