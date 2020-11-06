c883c782fc5eeab71e9e8a1f454acc2e103f5cfa
Ming Rui Zhang <mingrui.zhang@savoirfairelinux.com>
2020-11-06 10:52:21 -0500

    calladapter: initialize auto answer calls for non-current accounts
    Gitlab: #193
    Change-Id: I9ab1c298a1f720165affa55c0350bb27dfd67030

--- src/calladapter.h
+++ src/calladapter.h
@@ -65,6 +65,9 @@ public:
     Q_INVOKABLE void videoPauseThisCallToggle();
     Q_INVOKABLE bool isRecordingThisCall();
     Q_INVOKABLE QVariantList getConferencesInfos();
+    Q_INVOKABLE void updateCall(const QString& convUid = {},
+                                const QString& accountId = {},
+                                bool forceCallOnly = false);
 
 signals:
     void callStatusChanged(int index, const QString& accountId, const QString& convUid);
@@ -96,9 +99,6 @@ public slots:
     void slotAccountChanged();
 
 private:
-    void updateCall(const QString& convUid = {},
-                    const QString& accountId = {},
-                    bool forceCallOnly = false);
     bool shouldShowPreview(bool force);
     void showNotification(const QString& accountId, const QString& convUid);
 
