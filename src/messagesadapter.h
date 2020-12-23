f56072b9419b20cfbe341a03726dd775d2b9a6fd
ababi <albert.babi@savoirfairelinux.com>
2020-12-23 18:48:06 +0100

    swarm: add support for lazy loading conversations
    depends on https://review.jami.net/c/ring-lrc/+/16720
    
    Change-Id: Ic254459827c189fc92595be6e2485e0eed31edd8

--- src/messagesadapter.h
+++ src/messagesadapter.h
@@ -61,6 +61,7 @@ protected:
     Q_INVOKABLE void refuseFile(const QString& arg);
     Q_INVOKABLE void pasteKeyDetected();
     Q_INVOKABLE void onComposing(bool isComposing);
+    Q_INVOKABLE void loadMessages(int n);
 
     /*
      * Manually update draft when hiding message web view (Back to welcome page).
@@ -76,6 +77,9 @@ protected:
     void clear();
     void printHistory(lrc::api::ConversationModel& conversationModel,
                       MessagesList interactions);
+    void updateHistory(lrc::api::ConversationModel& conversationModel,
+                      MessagesList interactions,
+                      bool allLoaded);
     void setSenderImage(const QString& sender, const QString& senderImage);
     void printNewInteraction(lrc::api::ConversationModel& conversationModel,
                              const QString& msgId,
