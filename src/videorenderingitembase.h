6fd5026c19fa342f21e44ee1be37125cbcd066fe
Ming Rui Zhang <mingrui.zhang@savoirfairelinux.com>
2021-01-08 09:24:10 -0500

    video: QML OpenGL video customization
    Three implementation methods:
    
    1. QSGNode* updatePaintNode(QSGNode* old, UpdatePaintNodeData*)
    - Patchset: 13
    
    2. QQuickWindow::afterRendering
    - Patchset:14
    
    3. QQuickFramebufferObject
    - Patchset: 16 and onward
    
    Better copy-runtime-files post build link
    
    Separation of QRencode build script code
    
    Modified PhotoBooth, Record box, Preview/Distant render
    
    Modified conference layout
    
    RenderManager AvFrame Support while keeping old pipeline
    
    QQuickFramebufferObject, as the rendering base, uses OpenGL shader to render the video
    
    Gitlab: #282
    
    Change-Id: Ica41e433048098d200e9a2cbfdf0528d6f824208

---
+++ src/videorenderingitembase.h
@@ -0,0 +1,221 @@
+/*
+ * Copyright (C) 2020 by Savoir-faire Linux
+ * Author: Mingrui Zhang   <mingrui.zhang@savoirfairelinux.com>
+ * Author: Yang Wang <yang.wang@savoirfairelinux.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <QtQuick>
+#include <QWindow>
+#include <QMutex>
+#include <QMutexLocker>
+#include <QOpenGLFunctions>
+#include <QOpenGLTexture>
+#include <QOpenGLShaderProgram>
+#include <QOpenGLBuffer>
+
+class LRCInstance;
+
+extern "C" {
+struct AVFrame;
+}
+
+struct VideoRenderingItemBaseRenderConnections
+{
+    QMetaObject::Connection stopped, updated;
+};
+
+class VideoRenderingItemBase : public QQuickFramebufferObject
+{
+    Q_OBJECT
+
+public:
+    enum class Type { PREVIEW, DISTANT, PHOTO };
+    Q_ENUM(Type)
+
+    Q_PROPERTY(LRCInstance* lrcInstance MEMBER lrcInstance_ NOTIFY lrcInstanceChanged)
+    Q_PROPERTY(
+        VideoRenderingItemBase::Type renderingType READ getRenderingType WRITE setRenderingType)
+    Q_PROPERTY(QString distantRenderId READ getDistantRenderId WRITE setDistantRenderId NOTIFY
+                   distantRenderIdChanged)
+    Q_PROPERTY(
+        QSize expectedSize READ getExpectedSize WRITE setExpectedSize NOTIFY expectedSizeChanged)
+
+public:
+    explicit VideoRenderingItemBase(QQuickItem* parent = 0);
+    Renderer* createRenderer() const override;
+
+    Q_INVOKABLE void takePhoto();
+
+    // Setters
+    void setRenderingType(VideoRenderingItemBase::Type type);
+    void setNeedToUpdateFrame(bool update);
+    void setDistantRenderId(QString distantRenderId);
+    void setExpectedSize(QSize newSize);
+    void setCurrentTextureSize(const QSize& size);
+    void setRenderingFinished(bool finished);
+    void setAngleRotated(double angle);
+
+    // Getters
+    bool getNeedToUpdateFrame();
+    VideoRenderingItemBase::Type getRenderingType();
+    QString getDistantRenderId();
+    QSize getExpectedSize();
+    bool getRenderingFinished();
+    LRCInstance* getLrcInstance();
+
+    // Get scale factor to map frame size to actual item size
+    // used by conference layout
+    Q_INVOKABLE float getWidthScaleFactor();
+    Q_INVOKABLE float getHeightScaleFactor();
+
+signals:
+    void distantRenderIdChanged();
+    void expectedSizeChanged();
+    void currentTextureSizeChanged();
+    void updateParticipantsInfoRequest();
+    void photoIsReady(QString photoBase64);
+    void lrcInstanceChanged();
+
+private:
+    // Recalculate different size according to the changes of parameters
+    void recalculateItemSize();
+
+    // Connect to RenderManager
+    void connectRenderManager();
+
+    // Disconnect from RenderManager
+    void disconnectRenderManager();
+
+private:
+    VideoRenderingItemBase::Type renderingType_ {VideoRenderingItemBase::Type::PREVIEW};
+
+    QString distantRenderId_ {""};
+
+    QSharedPointer<QQuickItemGrabResult> photo_ {nullptr};
+
+    bool needToUpdateFrame_ {false};
+    bool renderingFinished_ {false};
+    double angleRotated_ {0.0};
+    QSize currentTextureSize_ {0, 0};
+    QSize expectedSize_ {0, 0};
+
+    // LRCInstance pointer
+    LRCInstance* lrcInstance_ {nullptr};
+
+    VideoRenderingItemBaseRenderConnections connections_;
+};
+
+class TexturesRenderer : public QObject, protected QOpenGLFunctions
+{
+    Q_OBJECT
+public:
+    explicit TexturesRenderer(VideoRenderingItemBase::Type type);
+    ~TexturesRenderer();
+
+    enum class TexturePlane { Y, U, V, NV12 };
+
+    // Setter
+    void setWindow(QQuickWindow* window)
+    {
+        window_ = window;
+    }
+
+    void setDistantRenderId(const QString& id)
+    {
+        distantRenderId_ = id;
+    }
+
+    // Getter
+    QSize getTextureSize()
+    {
+        return textureSize_;
+    }
+
+    double getAngleRotated()
+    {
+        return angleToRotateZ_;
+    }
+
+    void processData();
+
+    void updateFrame(LRCInstance* lrcInstance);
+    bool frameIsEmpty();
+
+    void initializeTextures();
+    void clearTextures();
+
+public slots:
+    // Main drawing function
+    void paint();
+
+private:
+    void initializeShaderProgram();
+    void initializeTextureDataYUV(QOpenGLTexture* texture);
+    void initializeTextureDataNV12(QOpenGLTexture* texture);
+
+    void processAllTexturesData();
+    void processTextureDataYUV(QOpenGLTexture* texture, TexturePlane plane);
+    void processTextureDataNV12();
+
+    void bindTextures();
+    void releaseTextures();
+
+    VideoRenderingItemBase::Type renderType_ {VideoRenderingItemBase::Type::PREVIEW};
+
+    QMutex drawMutex_;
+    QMutex textureMutex_;
+
+    QString vertexShaderFile_ {":/shader/YUVConv.vert"};
+    QString fragmentShaderFile_ {":/shader/YUVConv.frag"};
+    std::unique_ptr<QOpenGLShaderProgram> shaderProgram_ = nullptr;
+
+    QSize textureSize_;
+    QString distantRenderId_ {""};
+
+    // Current frame
+    AVFrame* frame_;
+
+    // Uniform values ids
+    int isNV12Id_ {0};
+    int angleToRotateYId_ {0};
+    int angleToRotateZId_ {0};
+    int lineSizeWidthScaleFactorsId_ {0};
+    int scaleFactorId_ {0};
+    GLuint uniformTextureSampler2DIds_[4] = {0};
+
+    // Attribute values ids
+    int vertsLocation_ {0};
+    int textureLocation_ {0};
+
+    // Uniform values to set into the shader
+    bool isNV12_ {false};
+    QVector2D scaleFactor_ {1.0, 1.0};
+    double angleToRotateZ_ {0.0};
+    double angleToRotateY_ {0.0};
+
+    // OpenGLTexture storage
+    std::unique_ptr<QOpenGLTexture> Ytex_ = nullptr;
+    std::unique_ptr<QOpenGLTexture> Utex_ = nullptr;
+    std::unique_ptr<QOpenGLTexture> Vtex_ = nullptr;
+    std::unique_ptr<QOpenGLTexture> UVtex_NV12_ = nullptr;
+
+    QMap<TexturePlane, QOpenGLPixelTransferOptions> options_;
+
+    // Item window pointer
+    QQuickWindow* window_ = nullptr;
+};
