0051324017a749e998c962eddbe552d0903767f6
ababi <albert.babi@savoirfairelinux.com>
2021-01-18 15:44:36 +0100

    misc: follow chatview and side panel design changes
    - Remove icons and resize side panel tab bar.
    
    - Change icons and style for chatview header and combo box buttons.
    
    - Update chatview colors.
    
    Gitlab: #241
    
    Change-Id: Ic5f4e0db5e1912c85cc4f658264371af8104cd8f

--- src/messagesadapter.h
+++ src/messagesadapter.h
@@ -77,7 +77,10 @@ protected:
      */
     void setMessagesVisibility(bool visible);
     void requestSendMessageContent();
-    void setInvitation(bool show, const QString& contactUri = "", const QString& contactId = "");
+    void setInvitation(bool show,
+                       const QString& contactUri = "",
+                       const QString& contactId = "",
+                       bool isSwarm = false);
     void clear();
     void printHistory(lrc::api::ConversationModel& conversationModel,
                       const QString& convUid,
@@ -99,9 +102,7 @@ protected:
     void setMessagesFileContent(const QString& path);
     void removeInteraction(const QString& interactionId);
     void setSendMessageContent(const QString& content);
-    void contactIsComposing(const QString& convUid,
-                            const QString& contactUri,
-                            bool isComposing);
+    void contactIsComposing(const QString& convUid, const QString& contactUri, bool isComposing);
 
 signals:
     void needToUpdateSmartList();
