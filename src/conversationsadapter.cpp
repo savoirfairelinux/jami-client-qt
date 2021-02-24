1fbd5b45e03e8da799d436e707d5f4f16d7b7a7d
ababi <albert.babi@savoirfairelinux.com>
2021-02-24 15:37:26 +0100

    smartlist: avoid updating smartlist on conversationRemoved
    Accessing smartlist on conversationRemoved can return invalid
    items. Instead, only show welcome view and use modelChanged signal
    for updating smartlist.
    
    Also avoid duplicate navigation to welcome view.
    
    Gitlab: #300
    
    Change-Id: Ic1c192fecceeb59287267e3445fabf6afb91ffd5

--- src/conversationsadapter.cpp
+++ src/conversationsadapter.cpp
@@ -245,7 +245,6 @@ ConversationsAdapter::connectConversationModel(bool updateFilter)
         = QObject::connect(currentConversationModel,
                            &lrc::api::ConversationModel::conversationRemoved,
                            [this]() {
-                               conversationSmartListModel_->fillConversationsList();
                                backToWelcomePage();
                            });
 
