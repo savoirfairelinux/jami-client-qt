ef72d4ca9fa2814f1e985fff61b922313cb04ac9
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2021-04-15 23:17:58 -0400

    (WIP)smartlist: split and improve interface with conversationmodel
    Change-Id: Ie8173504859b325374e42f0dbb4e0ae75f3ed740
    Gitlab: #373
    Gitlab: #374

---
+++ src/conversationlistmodel.cpp
@@ -0,0 +1,114 @@
+/*
+ * Copyright (C) 2021 by Savoir-faire Linux
+ * Author: Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
+ * Author: Mingrui Zhang <mingrui.zhang@savoirfairelinux.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "conversationlistmodel.h"
+
+ConversationListModel::ConversationListModel(LRCInstance* instance, QObject* parent)
+    : ConversationListModelBase(instance, parent)
+{
+    connect(
+        model_,
+        &ConversationModel::beginInsertRows,
+        this,
+        [this](int position, int rows) {
+            beginInsertRows(QModelIndex(), position, position + (rows - 1));
+        },
+        Qt::DirectConnection);
+    connect(model_,
+            &ConversationModel::endInsertRows,
+            this,
+            &ConversationListModel::endInsertRows,
+            Qt::DirectConnection);
+
+    connect(
+        model_,
+        &ConversationModel::beginRemoveRows,
+        this,
+        [this](int position, int rows) {
+            beginRemoveRows(QModelIndex(), position, position + (rows - 1));
+        },
+        Qt::DirectConnection);
+    connect(model_,
+            &ConversationModel::endRemoveRows,
+            this,
+            &ConversationListModel::endRemoveRows,
+            Qt::DirectConnection);
+
+    connect(model_, &ConversationModel::dataChanged, this, [this](int position) {
+        const auto index = createIndex(position, 0);
+        Q_EMIT ConversationListModel::dataChanged(index, index);
+    });
+}
+
+int
+ConversationListModel::rowCount(const QModelIndex& parent) const
+{
+    // For list models only the root node (an invalid parent) should return the list's size. For all
+    // other (valid) parents, rowCount() should return 0 so that it does not become a tree model.
+    if (!parent.isValid() && model_) {
+        return model_->getConversations().size();
+    }
+    return 0;
+}
+
+ConversationListModelBase::item_t
+ConversationListModel::itemFromIndex(const QModelIndex& index) const
+{
+    const auto& data = model_->getConversations();
+    if (!index.isValid() || data.empty())
+        return std::nullopt;
+    return std::make_optional(std::ref(data.at(index.row())));
+};
+
+ConversationListProxyModel::ConversationListProxyModel(QAbstractListModel* model, QObject* parent)
+    : SelectableListProxyModel(model, parent)
+{
+    setSortRole(Conversation::Role::LastInteractionTimeStamp);
+    sort(0, Qt::DescendingOrder);
+    setFilterCaseSensitivity(Qt::CaseSensitivity::CaseInsensitive);
+}
+
+bool
+ConversationListProxyModel::filterAcceptsRow(int sourceRow, const QModelIndex& sourceParent) const
+{
+    QModelIndex index = sourceModel()->index(sourceRow, 0, sourceParent);
+    auto itemProfileType = index.data(Conversation::Role::ContactType).toInt();
+    auto displayName = index.data(Conversation::Role::DisplayName).toString();
+    auto displayId = index.data(Conversation::Role::DisplayID).toString();
+    return (displayName.contains(filterRegExp()) || displayId.contains(filterRegExp()))
+           && static_cast<profile::Type>(itemProfileType) == currentTypeFilter_;
+}
+
+bool
+ConversationListProxyModel::lessThan(const QModelIndex& left, const QModelIndex& right) const
+{
+    QVariant leftData = sourceModel()->data(left, sortRole());
+    QVariant rightData = sourceModel()->data(right, sortRole());
+    // we're assuming the sort role data type here is some integral time
+    return leftData.toUInt() < rightData.toUInt();
+}
+
+void
+ConversationListProxyModel::setTypeFilter(const profile::Type& typeFilter)
+{
+    beginResetModel();
+    currentTypeFilter_ = typeFilter;
+    endResetModel();
+    updateSelection();
+};
