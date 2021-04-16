165d07bd3d8d6a9c96d19474f058744d8cbde42c
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2021-04-15 23:17:58 -0400

    sidepanel: improve smartlist interface with underlying models
    Minor cosmetic changes to the account combo box, search bar, filter
    tabs, and smartlist.
    
    Change-Id: Ie8173504859b325374e42f0dbb4e0ae75f3ed740
    Gitlab: #373
    Gitlab: #374
    Gitlab: #388

--- src/messagesadapter.cpp
+++ src/messagesadapter.cpp
@@ -163,13 +163,14 @@ MessagesAdapter::sendContactRequest()
     const auto convUid = lrcInstance_->get_selectedConvUid();
     if (!convUid.isEmpty()) {
         lrcInstance_->getCurrentConversationModel()->makePermanent(convUid);
+        qDebug() << "makePermanent " << convUid;
     }
 }
 
 void
 MessagesAdapter::updateConversationForAddedContact()
 {
-    auto* convModel = lrcInstance_->getCurrentConversationModel();
+    auto convModel = lrcInstance_->getCurrentConversationModel();
     const auto& convInfo = lrcInstance_->getConversationFromConvUid(
         lrcInstance_->get_selectedConvUid());
 
@@ -193,7 +194,6 @@ MessagesAdapter::slotSendMessageContentSaved(const QString& content)
     auto restoredContent = lrcInstance_->getContentDraft(lrcInstance_->get_selectedConvUid(),
                                                          lrcInstance_->getCurrAccId());
     setSendMessageContent(restoredContent);
-    Q_EMIT needToUpdateSmartList();
 }
 
 void
@@ -202,7 +202,6 @@ MessagesAdapter::slotUpdateDraft(const QString& content)
     if (!LastConvUid_.isEmpty()) {
         lrcInstance_->setContentDraft(LastConvUid_, lrcInstance_->getCurrAccId(), content);
     }
-    Q_EMIT needToUpdateSmartList();
 }
 
 void
@@ -460,11 +459,9 @@ MessagesAdapter::setConversationProfileData(const lrc::api::conversation::Info& 
     try {
         auto& contact = accInfo->contactModel->getContact(contactUri);
         auto bestName = accInfo->contactModel->bestNameForContact(contactUri);
-        setInvitation(contact.profileInfo.type == lrc::api::profile::Type::PENDING
-                          || contact.profileInfo.type == lrc::api::profile::Type::TEMPORARY,
+        setInvitation(contact.profileInfo.type == lrc::api::profile::Type::PENDING,
                       bestName,
                       contactUri);
-
         if (!contact.profileInfo.avatar.isEmpty()) {
             setSenderImage(contactUri, contact.profileInfo.avatar);
         } else {
