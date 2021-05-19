8b646f02d6ab8d58e35854e1e06ac63f5e85ee85
Trevor Tabah <trevor.tabah@savoirfairelinux.com>
2021-05-19 16:34:56 -0400

    Logs UI: created UI for clients to view logs
    Gitlab: #406
    
    Change-Id: I25dd7084165660e0a03c4694d0225bbcf0f1a9d6

--- src/settingsadapter.cpp
+++ src/settingsadapter.cpp
@@ -1100,3 +1100,14 @@ SettingsAdapter::isAllModeratorsEnabled(const QString& accountId)
 {
     return lrcInstance_->accountModel().isAllModerators(accountId);
 }
+
+void
+SettingsAdapter::monitorAndReceiveLogs(const bool& continuous)
+{
+    lrcInstance_->monitor(continuous);
+    QObject::connect(&lrcInstance_->behaviorController(),
+                     &lrc::api::BehaviorController::debugMessageReceived,
+                     this,
+                     &SettingsAdapter::debugMessageReceived,
+                     Qt::ConnectionType::UniqueConnection);
+}
