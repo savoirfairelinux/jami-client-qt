6e676a1986f403cb6a68ce221479c5cf1667bc4e
Ming Rui Zhang <mingrui.zhang@savoirfairelinux.com>
2021-05-26 16:35:07 -0400

    mainOverlay: add mosaic button
    Gitlab: #409
    
    Change-Id: I65e1c91184a95453c10633340a74f50cc4de7032

--- src/calladapter.cpp
+++ src/calladapter.cpp
@@ -629,6 +629,8 @@ CallAdapter::updateCallOverlay(const lrc::api::conversation::Info& convInfo)
     bool isAudioMuted = call->audioMuted && (call->status != lrc::api::call::Status::PAUSED);
     bool isVideoMuted = call->videoMuted && !isPaused && !call->isAudioOnly;
     bool isRecording = isRecordingThisCall();
+    bool isConferenceCall = call->participantsInfos.size() > 2;
+    bool isGrid = call->layout == lrc::api::call::Layout::GRID;
     auto bestName = convInfo.participants.isEmpty()
                         ? QString()
                         : accInfo.contactModel->bestNameForContact(convInfo.participants[0]);
@@ -639,6 +641,8 @@ CallAdapter::updateCallOverlay(const lrc::api::conversation::Info& convInfo)
                          isVideoMuted,
                          isRecording,
                          accInfo.profileInfo.type == lrc::api::profile::Type::SIP,
+                         isConferenceCall,
+                         isGrid,
                          bestName);
 }
 
@@ -710,6 +714,20 @@ CallAdapter::minimizeParticipant(const QString& uri)
         }
     } catch (...) {
     }
+}
+
+void
+CallAdapter::showGridConferenceLayout()
+{
+    auto* callModel = lrcInstance_->getAccountInfo(accountId_).callModel.get();
+    const auto& convInfo
+        = lrcInstance_->getConversationFromConvUid(lrcInstance_->get_selectedConvUid(), accountId_);
+
+    auto confId = convInfo.confId;
+    if (confId.isEmpty())
+        confId = convInfo.callId;
+
+    callModel->setConferenceLayout(confId, lrc::api::call::Layout::GRID);
 }
 
 void
