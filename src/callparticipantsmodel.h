aa04122e55eff004149376caaadf348deadd9636
agsantos <aline.gondimsantos@savoirfairelinux.com>
2021-06-01 10:24:54 -0400

    create call participants model
    in this patch the participants model is not used yet
    
    Change-Id: I20e31d6d91375c64038b298108296095a4229c6e

---
+++ src/callparticipantsmodel.h
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2021 by Savoir-faire Linux
+ * Author: Aline Gondim Santos <aline.gondimsantos@savoirfairelinux.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "lrcinstance.h"
+#include "qtutils.h"
+
+#include <QAbstractListModel>
+#include <QObject>
+#include <QQmlEngine>
+#include <QSortFilterProxyModel>
+#include <QQuickItem>
+#include <QJsonObject>
+
+#define CALL_PARTICIPANTS_ROLES \
+    X(Uri) \
+    X(Device) \
+    X(SinkId) \
+    X(BestName) \
+    X(Active) \
+    X(XPosition) \
+    X(YPosition) \
+    X(Width) \
+    X(Height) \
+    X(AudioLocalMuted) \
+    X(AUdioModeratorMuted) \
+    X(VideoMuted) \
+    X(IsModerator) \
+    X(IsLocal) \
+    X(IsContact)
+
+namespace CallParticipant {
+Q_NAMESPACE
+enum Role {
+    DummyRole = Qt::UserRole + 1,
+#define X(role) role,
+    CALL_PARTICIPANTS_ROLES
+#undef X
+};
+Q_ENUM_NS(Role)
+
+struct Item
+{
+    QJsonObject item;
+
+    bool operator==(const Item& a) const
+    {
+        return (item == a.item);
+    }
+};
+} // namespace CallParticipant
+
+class CallParticipantsModel : public QAbstractListModel
+{
+    Q_OBJECT
+public:
+    CallParticipantsModel(LRCInstance* instance, QObject* parent = nullptr);
+
+    int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
+    QHash<int, QByteArray> roleNames() const override;
+
+    void addParticipant(const CallParticipant::Item& item);
+    void removeParticipant(const CallParticipant::Item& item);
+    void clearParticipants();
+    void setParticipants(const QVariantList& participants);
+
+private:
+    LRCInstance* lrcInstance_ {nullptr};
+
+    QMap<QString, CallParticipant::Item> participants_;
+};
