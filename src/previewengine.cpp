e9b3b1b125b42ff590081028e889e83cd637977b
Trevor Tabah <trevor.tabah@savoirfairelinux.com>
2021-07-06 10:20:46 -0400

    messagelistmodel signals: add in client-qt
    Change-Id: Iedc40f6172a6cdacc48cda6f4187053fbf226713

---
+++ src/previewengine.cpp
@@ -0,0 +1,70 @@
+#include "previewengine.h"
+
+#include <QtWebEngine>
+#include <QWebEngineScript>
+#include <QWebEngineProfile>
+#include <QWebEngineSettings>
+
+PreviewEngine::PreviewEngine(QObject* parent)
+    : QWebEngineView(qobject_cast<QWidget*>(parent))
+    , pimpl_(new PreviewEnginePrivate(this))
+{
+    QWebEngineProfile* profile = QWebEngineProfile::defaultProfile();
+
+    QDir dataDir(QStandardPaths::writableLocation(QStandardPaths::AppLocalDataLocation));
+    dataDir.cdUp();
+    auto cachePath = dataDir.absolutePath() + "/jami";
+    profile->setCachePath(cachePath);
+    profile->setPersistentStoragePath(cachePath);
+    profile->setPersistentCookiesPolicy(QWebEngineProfile::NoPersistentCookies);
+    profile->setHttpCacheType(QWebEngineProfile::NoCache);
+
+    setPage(new QWebEnginePage(profile, this));
+
+    settings()->setAttribute(QWebEngineSettings::JavascriptEnabled, true);
+    settings()->setAttribute(QWebEngineSettings::JavascriptCanOpenWindows, true);
+    settings()->setAttribute(QWebEngineSettings::ScrollAnimatorEnabled, false);
+    settings()->setAttribute(QWebEngineSettings::ErrorPageEnabled, false);
+    settings()->setAttribute(QWebEngineSettings::PluginsEnabled, false);
+    settings()->setAttribute(QWebEngineSettings::FullScreenSupportEnabled, true);
+    settings()->setAttribute(QWebEngineSettings::ScreenCaptureEnabled, false);
+    settings()->setAttribute(QWebEngineSettings::LinksIncludedInFocusChain, false);
+    settings()->setAttribute(QWebEngineSettings::LocalStorageEnabled, false);
+    settings()->setAttribute(QWebEngineSettings::AllowRunningInsecureContent, true);
+    settings()->setAttribute(QWebEngineSettings::LocalContentCanAccessRemoteUrls, true);
+    settings()->setAttribute(QWebEngineSettings::XSSAuditingEnabled, false);
+    settings()->setAttribute(QWebEngineSettings::LocalContentCanAccessFileUrls, true);
+
+    settings()->setFontSize(QWebEngineSettings::MinimumFontSize, 2);
+
+    setContextMenuPolicy(Qt::ContextMenuPolicy::NoContextMenu);
+
+    channel_ = new QWebChannel(this);
+    channel_->registerObject(QStringLiteral("previewJSBridge"), pimpl_);
+
+    page()->setWebChannel(channel_);
+    page()->runJavaScript(Utils::QByteArrayFromFile(":/qwebchannel.js"),
+                          QWebEngineScript::MainWorld);
+    page()->runJavaScript(Utils::QByteArrayFromFile(":/previewInfo.js"),
+                          QWebEngineScript::MainWorld);
+}
+
+void
+PreviewEngine::beginPreviewProcess(int messageId, QString url)
+{
+    page()->runJavaScript(
+        QString("getPreviewInfo(`%1`, `%2`)").arg(QString(messageId), QString(url)));
+}
+
+void
+PreviewEnginePrivate::log(QString str)
+{
+    qDebug() << str;
+}
+
+void
+PreviewEnginePrivate::infoReady(int messageId, QVariantList previewInformation)
+{
+    qDebug() << "FIRST ELEMENT OF LIST " << previewInformation.at(0) << "\n";
+    Q_EMIT parent_->previewIsReady(messageId, previewInformation);
+}
---
+++ src/previewengine.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+
+#include "utils.h"
+
+#include <QtWebChannel>
+#include <QtWebEngine>
+#include <QtWebEngineCore>
+#include <QtWebEngine>
+#include <QWebEngineView>
+
+class PreviewEngine;
+
+class PreviewEnginePrivate : public QObject
+{
+    Q_OBJECT
+public:
+    PreviewEnginePrivate(PreviewEngine* parent)
+        : parent_(parent)
+    {}
+
+    Q_INVOKABLE void infoReady(int messageId, QVariantList info);
+    Q_INVOKABLE void log(QString str);
+
+private:
+    PreviewEngine* parent_;
+};
+
+class PreviewEngine : public QWebEngineView
+{
+    Q_OBJECT
+
+public:
+    explicit PreviewEngine(QObject* parent = nullptr);
+    void beginPreviewProcess(int messageId, QString url);
+
+Q_SIGNALS:
+    void emitPreviewInfo(int messageIndex, QString urlInMessage);
+    void testJSEmit(int messageIndex);
+    void previewIsReady(int messageId, QVariantList previewInfo);
+
+private:
+    QWebChannel* channel_;
+    PreviewEnginePrivate* pimpl_;
+};
