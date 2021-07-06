4566d8cc232f8712d5150c5bff081f530319b60d
Trevor Tabah <trevor.tabah@savoirfairelinux.com>
2021-07-06 10:20:46 -0400

    messagelistmodel signals: add in client-qt
    Change-Id: Iedc40f6172a6cdacc48cda6f4187053fbf226713

--- src/qmlregister.cpp
+++ src/qmlregister.cpp
@@ -28,6 +28,7 @@
 #include "deviceitemlistmodel.h"
 #include "smartlistmodel.h"
 #include "conversationlistmodelbase.h"
+#include "messagelistmodel.h"
 
 #include "appsettingsmanager.h"
 #include "distantrenderer.h"
@@ -105,6 +106,7 @@ registerTypes()
     QML_REGISTERTYPE(NS_MODELS, VideoFormatFpsModel);
     QML_REGISTERTYPE(NS_MODELS, PluginListPreferenceModel);
     QML_REGISTERTYPE(NS_MODELS, SmartListModel);
+    QML_REGISTERTYPE(NS_MODELS, MessageListModel);
 
     // Roles & type enums for models
     QML_REGISTERNAMESPACE(NS_MODELS, AccountList::staticMetaObject, "AccountList");
