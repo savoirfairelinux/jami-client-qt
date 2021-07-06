3ac0f77ffdb26f18759af5bd2f7d3b788bce4159
Trevor Tabah <trevor.tabah@savoirfairelinux.com>
2021-07-06 10:20:46 -0400

    chatview: replace web chat view with qml listview
    Gitlab: #467
    Change-Id: Iedc40f6172a6cdacc48cda6f4187053fbf226713

--- src/qmlregister.cpp
+++ src/qmlregister.cpp
@@ -24,6 +24,7 @@
 #include "contactadapter.h"
 #include "pluginadapter.h"
 #include "messagesadapter.h"
+#include "previewengine.h"
 #include "settingsadapter.h"
 #include "utilsadapter.h"
 #include "conversationsadapter.h"
@@ -65,6 +66,8 @@
 #include "api/pluginmodel.h"
 #include "api/conversation.h"
 
+//#include "messagelistmodel.h"
+
 #include <QMetaType>
 #include <QQmlEngine>
 
@@ -102,19 +105,20 @@ void
 registerTypes(QQmlEngine* engine,
               SystemTray* systemTray,
               LRCInstance* lrcInstance,
-              AppSettingsManager* appSettingsManager,
+              AppSettingsManager* settingsManager,
+              PreviewEngine* previewEngine,
               ScreenInfo* screenInfo,
               QObject* parent)
 {
     // setup the adapters (their lifetimes are that of MainApplication)
     auto callAdapter = new CallAdapter(systemTray, lrcInstance, parent);
-    auto messagesAdapter = new MessagesAdapter(appSettingsManager, lrcInstance, parent);
+    auto messagesAdapter = new MessagesAdapter(settingsManager, previewEngine, lrcInstance, parent);
     auto conversationsAdapter = new ConversationsAdapter(systemTray, lrcInstance, parent);
     auto avAdapter = new AvAdapter(lrcInstance, parent);
     auto contactAdapter = new ContactAdapter(lrcInstance, parent);
-    auto accountAdapter = new AccountAdapter(appSettingsManager, lrcInstance, parent);
+    auto accountAdapter = new AccountAdapter(settingsManager, lrcInstance, parent);
     auto utilsAdapter = new UtilsAdapter(systemTray, lrcInstance, parent);
-    auto settingsAdapter = new SettingsAdapter(appSettingsManager, lrcInstance, parent);
+    auto settingsAdapter = new SettingsAdapter(settingsManager, lrcInstance, parent);
     auto pluginAdapter = new PluginAdapter(lrcInstance, parent);
     auto currentConversation = new CurrentConversation(lrcInstance, parent);
     auto currentAccount = new CurrentAccount(lrcInstance, parent);
@@ -161,6 +165,7 @@ registerTypes(QQmlEngine* engine,
     QML_REGISTERTYPE(NS_MODELS, PluginListPreferenceModel);
     QML_REGISTERTYPE(NS_MODELS, FilesToSendListModel);
     QML_REGISTERTYPE(NS_MODELS, SmartListModel);
+    QML_REGISTERTYPE(NS_MODELS, MessageListModel);
 
     // Roles & type enums for models
     QML_REGISTERNAMESPACE(NS_MODELS, AccountList::staticMetaObject, "AccountList");
@@ -182,10 +187,10 @@ registerTypes(QQmlEngine* engine,
 
     QML_REGISTERSINGLETONTYPE_POBJECT(NS_CONSTANTS, screenInfo, "ScreenInfo")
     QML_REGISTERSINGLETONTYPE_POBJECT(NS_CONSTANTS, lrcInstance, "LRCInstance")
-    QML_REGISTERSINGLETONTYPE_POBJECT(NS_CONSTANTS, appSettingsManager, "AppSettingsManager")
+    QML_REGISTERSINGLETONTYPE_POBJECT(NS_CONSTANTS, settingsManager, "AppSettingsManager")
 
     auto avatarRegistry = new AvatarRegistry(lrcInstance, parent);
-    auto wizardViewStepModel = new WizardViewStepModel(lrcInstance, accountAdapter, appSettingsManager, parent);
+    auto wizardViewStepModel = new WizardViewStepModel(lrcInstance, accountAdapter, settingsManager, parent);
     QML_REGISTERSINGLETONTYPE_POBJECT(NS_HELPERS, avatarRegistry, "AvatarRegistry");
     QML_REGISTERSINGLETONTYPE_POBJECT(NS_MODELS, wizardViewStepModel, "WizardViewStepModel")
 
