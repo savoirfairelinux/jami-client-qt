ba2c05b050065b884d0a89303348122501a76aab
agsantos <aline.gondimsantos@savoirfairelinux.com>
2021-10-15 11:34:17 -0400

    videoInput: use new api
     - VideoSettings OK
     - PhotoBooth OK
     - Calls - mostly ok, have to test camera unplugin/replugin/multiplecameras
       -> seems to leak, client freezing in calls/conferences
     - Recording - OK
    
    Change-Id: I49362900979d48abf2507ab89fcb261808423445

--- src/previewrenderer.cpp
+++ src/previewrenderer.cpp
@@ -19,7 +19,6 @@
 
 #include "previewrenderer.h"
 
-#include "lrcinstance.h"
 
 PreviewRenderer::PreviewRenderer(QQuickItem* parent)
     : QQuickPaintedItem(parent)
@@ -32,9 +31,9 @@ PreviewRenderer::PreviewRenderer(QQuickItem* parent)
     connect(this, &PreviewRenderer::lrcInstanceChanged, [this] {
         if (lrcInstance_)
             previewFrameUpdatedConnection_ = connect(lrcInstance_->renderer(),
-                                                     &RenderManager::previewFrameUpdated,
-                                                     [this]() {
-                                                         if (isVisible())
+                                                     &RenderManager::distantFrameUpdated,
+                                                     [this](const QString& id) {
+                                                         if (rendererId_ == id && isVisible())
                                                              update(QRect(0, 0, width(), height()));
                                                      });
     });
@@ -49,7 +48,7 @@ void
 PreviewRenderer::paint(QPainter* painter)
 {
     lrcInstance_->renderer()
-        ->drawFrame(lrc::api::video::PREVIEW_RENDERER_ID, [this, painter](QImage* previewImage) {
+        ->drawFrame(rendererId_, [this, painter](QImage* previewImage) {
             if (previewImage) {
                 painter->setRenderHint(QPainter::Antialiasing);
                 painter->setRenderHint(QPainter::SmoothPixmapTransform);
@@ -104,7 +103,7 @@ void
 VideoCallPreviewRenderer::paint(QPainter* painter)
 {
     lrcInstance_->renderer()
-        ->drawFrame(lrc::api::video::PREVIEW_RENDERER_ID, [this, painter](QImage* previewImage) {
+        ->drawFrame(get_rendererId(), [this, painter](QImage* previewImage) {
             if (previewImage) {
                 auto scalingFactor = static_cast<qreal>(previewImage->height())
                                      / static_cast<qreal>(previewImage->width());
@@ -123,7 +122,7 @@ PhotoboothPreviewRender::PhotoboothPreviewRender(QQuickItem* parent)
     connect(this, &PreviewRenderer::lrcInstanceChanged, [this] {
         if (lrcInstance_)
             connect(lrcInstance_->renderer(),
-                    &RenderManager::previewRenderingStopped,
+                    &RenderManager::distantRenderingStopped,
                     this,
                     &PhotoboothPreviewRender::renderingStopped,
                     Qt::UniqueConnection);
@@ -133,7 +132,7 @@ PhotoboothPreviewRender::PhotoboothPreviewRender(QQuickItem* parent)
 QString
 PhotoboothPreviewRender::takePhoto(int size)
 {
-    if (auto previewImage = lrcInstance_->renderer()->getPreviewFrame()) {
+    if (auto previewImage = lrcInstance_->renderer()->getPreviewFrame(get_rendererId())) {
         return Utils::byteArrayToBase64String(Utils::QImageToByteArray(previewImage->copy()));
     }
     return {};
@@ -144,8 +143,7 @@ PhotoboothPreviewRender::paint(QPainter* painter)
 {
     painter->setRenderHint(QPainter::Antialiasing, true);
 
-    lrcInstance_->renderer()
-        ->drawFrame(lrc::api::video::PREVIEW_RENDERER_ID, [this, painter](QImage* previewImage) {
+    lrcInstance_->renderer()->drawFrame(get_rendererId(), [this, painter](QImage* previewImage) {
             if (previewImage) {
                 QImage scaledPreview;
                 scaledPreview = Utils::getCirclePhoto(*previewImage,
