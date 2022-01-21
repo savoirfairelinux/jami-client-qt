8602485c07cebe07cf09d6c7d78fe9720099f736
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2022-01-21 14:42:35 -0500

    main: use IPC to terminate secondary client during re-install(win)
    Change-Id: I79421eeab49c9ec0826010af99a364471bb81d1a

--- src/mainapplication.h
+++ src/mainapplication.h
@@ -57,6 +57,7 @@ private:
 class MainApplication : public QApplication
 {
     Q_OBJECT
+    Q_DISABLE_COPY(MainApplication)
 
 public:
     explicit MainApplication(int& argc, char** argv);
@@ -65,19 +66,34 @@ public:
     bool init();
     void restoreApp();
 
+    enum class Option {
+        StartMinimized = 0,
+        Debug,
+        DebugToConsole,
+        DebugToFile,
+        UpdateUrl,
+        MuteJamid,
+        TerminationRequested
+    };
+    QVariant getOpt(const Option opt)
+    {
+        return runOptions_[opt];
+    };
+
 private:
     void vsConsoleDebug();
     void fileDebug(QFile* debugFile);
-
     void loadTranslations();
     void initLrc(const QString& downloadUrl, ConnectivityMonitor* cm, bool logDaemon);
-    const QVariantMap parseArguments();
+    void parseArguments();
     void setApplicationFont();
     void initQmlLayer();
     void initSystray();
     void cleanup();
 
 private:
+    std::map<Option, QVariant> runOptions_;
+
     QScopedPointer<QFile> debugFile_;
     QScopedPointer<QQmlApplicationEngine> engine_;
     QScopedPointer<LRCInstance> lrcInstance_;
