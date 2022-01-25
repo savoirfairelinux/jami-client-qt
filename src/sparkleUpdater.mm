6e1c2cdcd1d5c46ee498ea3d59758672025d6734
kkostiuk <kateryna.kostiuk@savoirfairelinux.com>
2022-01-25 11:56:34 -0500

    updater: add sparkle
    - add sparkle submodule
    - add option to enable sparkle
    - modify entitlements
    
    Gitlab: #578
    Change-Id: I7f562112a72a33e008ab316479fbaa68dc0e07f1

---
+++ src/sparkleUpdater.mm
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2021-2022 by Savoir-faire Linux
+ * Author: Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "sparkleupdater.h"
+
+#include <Sparkle/Sparkle.h>
+
+SPUStandardUpdaterController* updaterController;
+
+SparkleUpdater::SparkleUpdater()
+{
+    updaterController = [[SPUStandardUpdaterController alloc] initWithStartingUpdater: true updaterDelegate: nil userDriverDelegate: nil];
+}
+
+SparkleUpdater::~SparkleUpdater(){}
+
+void SparkleUpdater::checkForUpdates()
+{
+    [updaterController checkForUpdates: nil];
+}
+
+void SparkleUpdater::automaticallyCheckForUpdates(bool state)
+{
+    updaterController.updater.automaticallyChecksForUpdates = state;
+}
+
+bool SparkleUpdater::isAutoUpdaterEnabled()
+{
+    return updaterController.updater.automaticallyChecksForUpdates;
+}
