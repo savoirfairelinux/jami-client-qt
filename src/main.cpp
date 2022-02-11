dfae73d0d66b926b1786423310cccef8d542f2ae
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2022-02-11 12:11:38 -0500

    misc: remove code duplication and use native theme on KDE
    GitLab: #603
    Change-Id: I271f6d4b9b8089dc88479f1d7d98514acb3d52be

--- src/main.cpp
+++ src/main.cpp
@@ -62,7 +62,9 @@ main(int argc, char* argv[])
     QList<char*> qtWebEngineChromiumFlags;
 
 #ifdef Q_OS_LINUX
-    setenv("QT_QPA_PLATFORMTHEME", "gtk3", true);
+    if (!getenv("QT_QPA_PLATFORMTHEME")
+        && !(getenv("XDG_CURRENT_DESKTOP") == "KDE" || getenv("XDG_CURRENT_DESKTOP") == "GNOME"))
+        setenv("QT_QPA_PLATFORMTHEME", "gtk3", true);
     setenv("QML_DISABLE_DISK_CACHE", "1", true);
 
     /*
