ad9e9d95f9509304cc718938bea308011f257457
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2022-02-11 09:25:33 -0500

    video: use QVideoSink/VideoOutput and QVideoFrame instead of QImage
    Change-Id: I2391a32294922ea435ab80ac1f876c004ff6c21e

---
+++ src/videoprovider.h
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2022 Savoir-faire Linux Inc.
+ * Author: Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "utils.h"
+#include "qtutils.h"
+
+#include "api/avmodel.h"
+
+extern "C" {
+#include "libavutil/frame.h"
+}
+
+#include <QVideoSink>
+#include <QVideoFrame>
+#include <QQmlEngine>
+#include <QMutex>
+
+using namespace lrc::api;
+
+class VideoProvider final : public QObject
+{
+    Q_OBJECT
+    QML_ELEMENT
+    QML_PROPERTY(QVariantMap, activeRenderers)
+public:
+    explicit VideoProvider(AVModel& avModel, QObject* parent = nullptr);
+    ~VideoProvider() = default;
+
+    Q_INVOKABLE void registerSink(const QString& id, QVideoSink* obj);
+    Q_INVOKABLE void unregisterSink(QVideoSink* obj);
+    Q_INVOKABLE QString captureVideoFrame(QVideoSink* obj);
+
+private Q_SLOTS:
+    void onRendererStarted(const QString& id);
+    void onFrameBufferRequested(const QString& id, AVFrame* avframe);
+    void onFrameUpdated(const QString& id);
+    void onRendererStopped(const QString& id);
+
+private:
+    AVModel& avModel_;
+
+    struct FrameObject
+    {
+        std::unique_ptr<QVideoFrame> videoFrame;
+        QMutex mutex;
+        QSet<QVideoSink*> subscribers;
+    };
+    std::map<QString, std::unique_ptr<FrameObject>> framesObjects_;
+    FrameObject* getFrameObject(const QString& id)
+    {
+        auto it = framesObjects_.find(id);
+        if (it != framesObjects_.end()) {
+            return it->second.get();
+        }
+        return nullptr;
+    };
+};
