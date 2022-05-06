b793e227f11ecc63d2594e30b1136ef6400e35d1
Amin Bandali <amin.bandali@savoirfairelinux.com>
2022-05-06 16:00:16 -0400

    misc: vendor libjamiclient into 'src/libjamiclient/'
    This is libjamiclient from the jami-libclient repository
    as of c5bca2933fb02ef7f81e8fe0772801a4f97fd1f5.
    
    GitLab: #734
    Change-Id: I86e3b0fb30e554755023e7b858b6a0d132cd59ab

---
+++ src/libjamiclient/interfaces/pixmapmanipulatori.h
@@ -0,0 +1,105 @@
+/****************************************************************************
+ *    Copyright (C) 2013-2022 Savoir-faire Linux Inc.                       *
+ *   Author : Emmanuel Lepage Vallee <emmanuel.lepage@savoirfairelinux.com> *
+ *                                                                          *
+ *   This library is free software; you can redistribute it and/or          *
+ *   modify it under the terms of the GNU Lesser General Public             *
+ *   License as published by the Free Software Foundation; either           *
+ *   version 2.1 of the License, or (at your option) any later version.     *
+ *                                                                          *
+ *   This library is distributed in the hope that it will be useful,        *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
+ *   Lesser General Public License for more details.                        *
+ *                                                                          *
+ *   You should have received a copy of the GNU General Public License      *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ ***************************************************************************/
+#pragma once
+
+#include <typedefs.h>
+
+// Qt
+class QVariant;
+class QModelIndex;
+class QByteArray;
+
+// Ring
+struct UserActionElement;
+
+namespace lrc {
+namespace api {
+namespace account {
+struct Info;
+}
+namespace conversation {
+struct Info;
+}
+} // namespace api
+} // namespace lrc
+
+namespace Interfaces {
+
+/**
+ * Different clients can have multiple way of displaying images. Some may
+ * add borders, other add corner radius (see Ubuntu-SDK HIG). This
+ * abstract class define many operations that can be defined by each clients.
+ *
+ * Most methods return QVariants as this library doesn't link against QtGui
+ *
+ * This interface is not frozen, more methods may be added later.
+ */
+class PixmapManipulatorI
+{
+public:
+    // Implementation can use random values to extend this
+    enum CollectionIconHint {
+        NONE,
+        HISTORY,
+        CONTACT,
+        BOOKMARK,
+        PHONE_NUMBER,
+        RINGTONE,
+        PROFILE,
+        CERTIFICATE,
+        ACCOUNT,
+        RECORDING,
+        MACRO,
+    };
+
+    virtual ~PixmapManipulatorI() = default;
+
+    virtual QVariant conversationPhoto(const lrc::api::conversation::Info& conversation,
+                                       const lrc::api::account::Info& accountInfo,
+                                       const QSize& size,
+                                       bool displayPresence = true)
+    {
+        Q_UNUSED(conversation);
+        Q_UNUSED(accountInfo);
+        Q_UNUSED(size);
+        Q_UNUSED(displayPresence);
+        return {};
+    }
+    virtual QVariant numberCategoryIcon(const QVariant& p,
+                                        const QSize& size,
+                                        bool displayPresence = false,
+                                        bool isPresent = false)
+        = 0;
+    virtual QByteArray toByteArray(const QVariant& pxm) = 0;
+    virtual QVariant personPhoto(const QByteArray& data, const QString& type = "PNG") = 0;
+    virtual QVariant decorationRole(const QModelIndex& index) = 0;
+    virtual QVariant decorationRole(const lrc::api::conversation::Info& conversation,
+                                    const lrc::api::account::Info& accountInfo)
+    {
+        Q_UNUSED(conversation);
+        Q_UNUSED(accountInfo);
+        return {};
+    }
+
+    /**
+     * Return the icons associated with the action and its state
+     */
+    virtual QVariant userActionIcon(const UserActionElement& state) const = 0;
+};
+
+} // namespace Interfaces
---
+++ src/libjamiclient/qtwrapper/pluginmanagerMock.cpp
@@ -0,0 +1,148 @@
+/******************************************************************************
+ *    Copyright (C) 2014-2022 Savoir-faire Linux Inc.                         *
+ *   Author : Aline Gondim Santos <aline.gondimsantos@savoirfairelinux.com>   *
+ *                                                                            *
+ *   This library is free software; you can redistribute it and/or            *
+ *   modify it under the terms of the GNU Lesser General Public               *
+ *   License as published by the Free Software Foundation; either             *
+ *   version 2.1 of the License, or (at your option) any later version.       *
+ *                                                                            *
+ *   This library is distributed in the hope that it will be useful,          *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU        *
+ *   Lesser General Public License for more details.                          *
+ *                                                                            *
+ *   You should have received a copy of the Lesser GNU General Public License *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.    *
+ *****************************************************************************/
+#pragma once
+
+#include "pluginmanager_wrap.h"
+
+bool
+PluginManagerInterface::loadPlugin(const QString& path)
+{
+    return false;
+}
+
+bool
+PluginManagerInterface::unloadPlugin(const QString& path)
+{
+    return false;
+}
+
+MapStringString
+PluginManagerInterface::getPluginDetails(const QString& path)
+{
+    return {};
+}
+
+QStringList
+PluginManagerInterface::getInstalledPlugins()
+{
+    return {};
+}
+
+QStringList
+PluginManagerInterface::getLoadedPlugins()
+{
+    return {};
+}
+
+int
+PluginManagerInterface::installPlugin(const QString& jplPath, bool force)
+{
+    return 0;
+}
+
+int
+PluginManagerInterface::uninstallPlugin(const QString& pluginRootPath)
+{
+    return 0;
+}
+
+QStringList
+PluginManagerInterface::getCallMediaHandlers()
+{
+    return {};
+}
+
+void
+PluginManagerInterface::toggleCallMediaHandler(const QString& mediaHandlerId,
+                                               const QString& callId,
+                                               bool toggle)
+{}
+
+QStringList
+PluginManagerInterface::getChatHandlers()
+{
+    return {};
+}
+
+void
+PluginManagerInterface::toggleChatHandler(const QString& chatHandlerId,
+                                          const QString& accountId,
+                                          const QString& peerId,
+                                          bool toggle)
+{}
+
+QStringList
+PluginManagerInterface::getCallMediaHandlerStatus(const QString& callId)
+{
+    return {};
+}
+
+MapStringString
+PluginManagerInterface::getCallMediaHandlerDetails(const QString& mediaHandlerId)
+{
+    return {};
+}
+
+QStringList
+PluginManagerInterface::getChatHandlerStatus(const QString& accountId, const QString& peerId)
+{
+    return {};
+}
+
+MapStringString
+PluginManagerInterface::getChatHandlerDetails(const QString& chatHandlerId)
+{
+    return {};
+}
+
+void
+PluginManagerInterface::setPluginsEnabled(bool enable)
+{}
+
+bool
+PluginManagerInterface::getPluginsEnabled()
+{
+    return false;
+}
+
+VectorMapStringString
+PluginManagerInterface::getPluginPreferences(const QString& path, const QString& accountId)
+{
+    return {};
+}
+
+bool
+PluginManagerInterface::setPluginPreference(const QString& path,
+                                            const QString& accountId,
+                                            const QString& key,
+                                            const QString& value)
+{
+    return false;
+}
+
+MapStringString
+PluginManagerInterface::getPluginPreferencesValues(const QString& path, const QString& accountId)
+{
+    return {};
+}
+
+bool
+PluginManagerInterface::resetPluginPreferencesValues(const QString& path, const QString& accountId)
+{
+    return false;
+}
