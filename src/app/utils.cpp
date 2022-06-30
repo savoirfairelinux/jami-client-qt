14157fc9faa72159aa500170fd3246b215dc3178
Fadi SHEHADEH <fadi.shehadeh@savoirfairelinux.com>
2022-06-30 11:58:41 -0400

    about Jami: window style and content
    - changed flickable window to rectangle
    - font size
    - window radius
    - hyperlink to company name
    - changed escription
    - added close button
    - deleted html reformating after file load and added it directly to the html file containing the credits
    
    Change-Id: I7805ddead153c9be4d66e7789e754ba2c3cc20a7
    Gitlab: #731

--- src/app/utils.cpp
+++ src/app/utils.cpp
@@ -574,28 +574,8 @@ Utils::getProjectCredits()
     QTextStream in(&projectCreditsFile);
     while (!in.atEnd()) {
         QString currentLine = in.readLine();
-        if (currentLine.contains("Created by:")) {
-            credits += "<h3 align=\"center\" style=\" margin-top:0px; "
-                       + QString("margin-bottom:0px; margin-left:0px; margin-right:0px; ")
-                       + "-qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600;\">"
-                       + QObject::tr("Created by:") + "</span></h3>";
-            continue;
-        } else if (currentLine.contains("Artwork by:")) {
-            credits
-                += "<h3 align=\"center\" style=\" margin-top:0px; margin-bottom:0px; "
-                   + QString(
-                       "margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">")
-                   + "<span style=\" font-weight:600;\">" + QObject::tr("Artwork by:")
-                   + "</span></h3>";
-            continue;
-        }
         credits += currentLine;
     }
-    credits += "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; "
-               + QString(
-                   "margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">")
-               + QObject::tr("Based on the SFLPhone project") + "</p>";
-
     return credits;
 }
 
