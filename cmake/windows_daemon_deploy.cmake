948c7bda3647f0fbcd40f65236cbf5413a8bef55
Aline Gondim Santos <aline.gondimsantos@savoirfairelinux.com>
2022-07-21 19:08:12 -0300

    misc: use static ffmpeg
    Change-Id: I3880d4b45de98377f4a2779723522a7ed1de8527

--- cmake/windows_daemon_deploy.cmake
+++ cmake/windows_daemon_deploy.cmake
@@ -2,14 +2,7 @@ if (EXISTS ${TIME_STAMP_FILE})
     message("No need for daemon deployment")
 else()
     message("Daemon deploying ...")
-    file(COPY "${DRING_PATH}/contrib/build/ffmpeg/Build/win32/x64/bin/avcodec-58.dll"
-              "${DRING_PATH}/contrib/build/ffmpeg/Build/win32/x64/bin/avutil-56.dll"
-              "${DRING_PATH}/contrib/build/ffmpeg/Build/win32/x64/bin/avformat-58.dll"
-              "${DRING_PATH}/contrib/build/ffmpeg/Build/win32/x64/bin/avdevice-58.dll"
-              "${DRING_PATH}/contrib/build/ffmpeg/Build/win32/x64/bin/swresample-3.dll"
-              "${DRING_PATH}/contrib/build/ffmpeg/Build/win32/x64/bin/swscale-5.dll"
-              "${DRING_PATH}/contrib/build/ffmpeg/Build/win32/x64/bin/avfilter-7.dll"
-              "${DRING_PATH}/contrib/build/openssl/libcrypto-1_1-x64.dll"
+    file(COPY "${DRING_PATH}/contrib/build/openssl/libcrypto-1_1-x64.dll"
               "${DRING_PATH}/contrib/build/openssl/libssl-1_1-x64.dll"
               "${PROJECT_ROOT_DIR}/qt.conf"
               "${PROJECT_ROOT_DIR}/resources/images/jami.ico"
