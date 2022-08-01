846f6aa42274f26d0ad4328494bc3c4e873214a8
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2022-08-01 17:40:55 -0400

    translations: fix powershell script to support vs 2017/2019 Qt
    Change-Id: I7d36fccee7b34b21a68cd9808fb3cc46b1d0f7d9

--- update-translations.ps1
+++ update-translations.ps1
@@ -1,15 +1,13 @@
 [cmdletbinding()]
-param (
-    [string]$qtver
-);
+param ([string]$qtver);
 
 $clientDir = split-path -parent $MyInvocation.MyCommand.Definition
 $qtver = If ($qtver) { $qtver } Else { "5.15.0" }
-$qtMinorVer = $qtver.split('.')[1]
-$qtMsvcDir = If (([int]$qtMinorVer) -le 14) {"msvc2017_64"} Else {"msvc2019_64"}
-$QtDir = "C:\Qt\$qtver\$qtMsvcDir"
-
-$tsFileNames = Get-ChildItem -Path "$clientDir\translations" -Recurse -Include *.ts
+$QtDir = "C:\Qt\$qtver\msvc2019_64"
+if (-not(Test-Path -Path $QtDir)) {
+    $QtDir = "C:\Qt\$qtver\msvc2017_64"
+}
 $lupdate = "$QtDir\bin\lupdate.exe"
 
+$tsFileNames = Get-ChildItem -Path "$clientDir\translations" -Recurse -Include *.ts
 Invoke-Expression("$lupdate $clientDir\src -ts $tsFileNames -no-obsolete")
