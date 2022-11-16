1617727f0fef7773e6b116b9d1084069d666e4c2
Aline Gondim Santos <aline.gondimsantos@savoirfairelinux.com>
2022-11-16 16:02:59 -0300

    windows: add support for system theme
    Use registry
    "HKEY_CURRENT_USER/Software/Microsoft/Windows/CurrentVersion
    /Themes/Personalize/AppsUseLightTheme"
    to check if system theme is supported and if it is dark or
    light.
    
    Removes "EnableDarkTheme" in favor of "AppTheme".
    
    Requires Windows SDK version 10.0.18362.0.
    
    GitLab: #723
    
    Change-Id: Ice8f7936a90535f47dc1870d4f18215e062684ba

--- src/app/utilsadapter.h
+++ src/app/utilsadapter.h
@@ -33,6 +33,15 @@
 #include <gio/gio.h>
 #endif
 
+#ifdef WIN32
+#define WATCHSYSTEMTHEME \
+    __has_include(<winrt/Windows.Foundation.h>) && __has_include(<winrt/Windows.Foundation.h>)
+#endif
+#if WATCHSYSTEMTHEME
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.UI.ViewManagement.h>
+#endif
+
 class QClipboard;
 class SystemTray;
 
@@ -112,14 +121,7 @@ public:
                                                           const QString& uri);
     Q_INVOKABLE bool luma(const QColor& color) const;
     Q_INVOKABLE bool useApplicationTheme();
-    Q_INVOKABLE bool hasNativeDarkTheme() const
-    {
-#if __has_include(<gio/gio.h>)
-        return true;
-#else
-        return false;
-#endif
-    }
+    Q_INVOKABLE bool hasNativeDarkTheme() const;
 
 Q_SIGNALS:
     void debugMessageReceived(const QString& message);
@@ -139,6 +141,10 @@ private:
 #if __has_include(<gio/gio.h>)
     GSettings* settings {nullptr};
     GSettingsSchema* schema {nullptr};
+#endif
+
+#if WATCHSYSTEMTHEME
+    winrt::Windows::UI::ViewManagement::UISettings settings;
 #endif
 };
 Q_DECLARE_METATYPE(UtilsAdapter*)
