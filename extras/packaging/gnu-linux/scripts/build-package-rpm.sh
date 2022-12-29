4b09b592af67cb54e4164c335d3cfa091cb1834a
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2022-12-29 09:40:06 -0500

    misc: fix right issues
    Change-Id: I5eae54d683862154a873787c3d8e1b73b55b0beb

--- extras/packaging/gnu-linux/scripts/build-package-rpm.sh
+++ extras/packaging/gnu-linux/scripts/build-package-rpm.sh
@@ -145,5 +145,8 @@ mv /root/rpmbuild/RPMS/*/* /opt/output
 touch /opt/output/.packages-built
 chown -R "$CURRENT_UID:$CURRENT_UID" /opt/output
 
+chown ${CURRENT_UID}:${CURRENT_GID} /opt/output/*.snap
+chown -R "${CURRENT_UID}:${CURRENT_UID}" .
+
 # TODO: One click install: create a package that combines the already
 # built package into one.
