30d0560765561fb9730ea8d647f2277327c6fac6
Amin Bandali <amin.bandali@savoirfairelinux.com>
2023-01-25 21:27:01 -0500

    packaging: Update release tarball name and version scheme.
    * extras/packaging/gnu-linux/scripts/release-version.sh: Add script
    for deriving (reproducibly) a release version.  The format will follow
    a 'YYYYMMDD.X' scheme, where 'YYYYMMDD' is the date of the last commit
    on the current branch, and 'X' the number of releases tagged that day,
    starting from zero.  Example: the first stable release with a newest
    commit date of 2023-01-02 would have a version of '20230102.0', the
    subsequent one '20230102.1', and so on.  This is derived by counting
    the number of git tags with the prefix 'TYPE/YYYYMMDD', where 'TYPE'
    is the release type: either 'stable', 'beta', or 'nightly'.
    * extras/packaging/gnu-linux/Jenkinsfile: Call the above new script to
    derive the release version to use for tagging.
    * extras/packaging/gnu-linux/Makefile: Switch from the previous
    'jami_YYYYMMDD.hhmm.COMMIT.tar.gz' tarball filename format to
    'jami-YYYYMMDD.X.tar.gz'.  The new format is shorter, omits the
    unnecessary and confusing commit id, and uses dash instead of
    underscore.  Also, it contains everything in a top-level directory
    with the same name as the tarball (without the .tar.gz extensions),
    so that 'jami-YYYYMMDD.X.tar.gz' nicely extracts to 'jami-YYYYMMDD.X'.
    With these changes, our release tarballs will follow more closely the
    related instructions from the GNU maintainers manual:
    https://www.gnu.org/prep/maintain/html_node/Distribution-tar-Files.html
    Lastly, this change inadvertently 'fixes' a recent issue where the
    'extras/ci/client-qt-gnulinux' subdirectory and its contents would be
    moved to top-level in the tarball due to one of the tarball transforms
    matching the 'client-qt' in the directory's name, replacing the
    'extras/ci/client-qt' prefix with 'client-qt', thereby resulting in
    'client-qt-gnulinux' being moved to the tarball's top-level.
    * .gitignore: Ignore tarballs.manifest and jami-*.tar.gz files.
    * extras/packaging/gnu-linux/tarballs.manifest: Drop file accidentally
    checked into git.
    
    Change-Id: I2695f560c0bc5e61fb99dc0244f8fbb8e7026a62

---
+++ extras/packaging/gnu-linux/scripts/release-version.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+#
+# Copyright (C) 2023 Savoir-faire Linux Inc.
+#
+# Author: Amin Bandali <amin.bandali@savoirfairelinux.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# This script is used in the packaging containers to build a snap
+# package on an ubuntu base distro.
+
+if [ $# -gt 1 ]; then
+    echo "Usage: $0 {stable,beta,nightly}"
+    exit 1
+fi
+
+# default to stable if no release type given
+release_type=${1:-stable}
+
+last_commit_date=$(git log -1 --format=%cd --date=format:'%Y%m%d')
+same_day_releases=$(git tag -l "${release_type}/${last_commit_date}*" | wc -l)
+release_counter=${same_day_releases:-0}
+release_version=${last_commit_date}.${release_counter}
+printf "${release_version}"
