0f725fef5e1e1a9e3d4b29dd09e5993f24c35015
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2023-02-26 20:32:34 -0500

    macos: support unified build
    This patch adds a parameter arch to specify what architecture to
    build. Arch could be arm64, x86_64 or unified.
    
    Change-Id: I2907e03410e0c01b8505afbe283da04b8d0291b6

--- extras/scripts/install.sh
+++ extras/scripts/install.sh
@@ -28,6 +28,7 @@ export OSTYPE
   # -u: disable use of privileges (sudo) during install
   # -W: disable libwrap and shared library
   # -w: do not use Qt WebEngine
+  # -a: arch to build
 
 set -ex
 
@@ -42,8 +43,9 @@ proc='1'
 priv_install=true
 enable_libwrap=true
 enable_webengine=true
+arch=''
 
-while getopts gsc:dQ:P:p:uWw OPT; do
+while getopts gsc:dQ:P:p:uWw:a: OPT; do
   case "$OPT" in
     g)
       global='true'
@@ -71,6 +73,9 @@ while getopts gsc:dQ:P:p:uWw OPT; do
     ;;
     w)
       enable_webengine='false'
+    ;;
+    a)
+      arch="${OPTARG}"
     ;;
     \?)
       exit 1
@@ -102,64 +107,68 @@ fi
 
 # jamid
 DAEMON="${TOP}/daemon"
-cd "$DAEMON"
-
-# Build the contribs.
-mkdir -p contrib/native
-(
-    cd contrib/native
-    ../bootstrap ${prefix:+"--prefix=$prefix"}
-    make -j"${proc}"
-)
-
-if [[ "${enable_libwrap}" != "true" ]]; then
-  # Disable shared if requested
-  if [[ "$OSTYPE" != "darwin"* ]]; then
-    CONFIGURE_FLAGS+=" --disable-shared"
-  fi
-fi
-
-BUILD_TYPE="Release"
-if [ "${debug}" = "true" ]; then
-  BUILD_TYPE="Debug"
-  CONFIGURE_FLAGS+=" --enable-debug"
-fi
-
-# Build the daemon itself.
-test -f configure || ./autogen.sh
-
-if [ "${global}" = "true" ]; then
-    ./configure ${CONFIGURE_FLAGS} ${prefix:+"--prefix=$prefix"}
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sh ${TOP}/extras/scripts/mac_dependencies_build.sh -a "$arch"
 else
-    ./configure ${CONFIGURE_FLAGS} --prefix="${INSTALL_DIR}"
-fi
-make -j"${proc}" V=1
-make_install "${global}" "${priv_install}"
+    cd "$DAEMON"
 
-# Verify system's version if no path provided.
-if [ -z "$qtpath" ]; then
-    sys_qtver=""
-    if command -v qmake6 &> /dev/null; then
-        sys_qtver=$(qmake6 -v)
-    elif command -v qmake-qt6 &> /dev/null; then
-        sys_qtver=$(qmake-qt6 -v) # Fedora
-    elif command -v qmake &> /dev/null; then
-        sys_qtver=$(qmake -v)
-    else
-        echo "No valid Qt found"; exit 1;
+    # Build the contribs.
+    mkdir -p contrib/native
+    (
+        cd contrib/native
+        ../bootstrap ${prefix:+"--prefix=$prefix"}
+        make -j"${proc}"
+    )
+
+    if [[ "${enable_libwrap}" != "true" ]]; then
+      # Disable shared if requested
+      if [[ "$OSTYPE" != "darwin"* ]]; then
+        CONFIGURE_FLAGS+=" --disable-shared"
+      fi
     fi
 
-    sys_qtver=${sys_qtver#*Qt version}
-    sys_qtver=${sys_qtver%\ in\ *}
+    BUILD_TYPE="Release"
+    if [ "${debug}" = "true" ]; then
+      BUILD_TYPE="Debug"
+      CONFIGURE_FLAGS+=" --enable-debug"
+    fi
 
-    installed_qtver=$(echo "$sys_qtver" | cut -d'.' -f 2)
-    required_qtver=$(echo $QT_MIN_VER | cut -d'.' -f 2)
+    # Build the daemon itself.
+    test -f configure || ./autogen.sh
 
-    if [[ $installed_qtver -ge $required_qtver ]] ; then
-        # Set qtpath to empty in order to use system's Qt.
-        qtpath=""
+    if [ "${global}" = "true" ]; then
+        ./configure ${CONFIGURE_FLAGS} ${prefix:+"--prefix=$prefix"}
     else
-        echo "No valid Qt found"; exit 1;
+        ./configure ${CONFIGURE_FLAGS} --prefix="${INSTALL_DIR}"
+    fi
+    make -j"${proc}" V=1
+    make_install "${global}" "${priv_install}"
+
+    # Verify system's version if no path provided.
+    if [ -z "$qtpath" ]; then
+        sys_qtver=""
+        if command -v qmake6 &> /dev/null; then
+            sys_qtver=$(qmake6 -v)
+        elif command -v qmake-qt6 &> /dev/null; then
+            sys_qtver=$(qmake-qt6 -v) # Fedora
+        elif command -v qmake &> /dev/null; then
+            sys_qtver=$(qmake -v)
+        else
+            echo "No valid Qt found"; exit 1;
+        fi
+
+        sys_qtver=${sys_qtver#*Qt version}
+        sys_qtver=${sys_qtver%\ in\ *}
+
+        installed_qtver=$(echo "$sys_qtver" | cut -d'.' -f 2)
+        required_qtver=$(echo $QT_MIN_VER | cut -d'.' -f 2)
+
+        if [[ $installed_qtver -ge $required_qtver ]] ; then
+            # Set qtpath to empty in order to use system's Qt.
+            qtpath=""
+        else
+            echo "No valid Qt found"; exit 1;
+        fi
     fi
 fi
 
@@ -172,6 +181,21 @@ client_cmake_flags=(-DCMAKE_BUILD_TYPE="${BUILD_TYPE}"
                     -DCMAKE_PREFIX_PATH="${qtpath}"
                     -DENABLE_LIBWRAP="${enable_libwrap}"
                     -DWITH_WEBENGINE="${enable_webengine}")
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    #detect arch for macos
+    CMAKE_OSX_ARCHITECTURES="arm64"
+    if [[ "$arch" == 'unified' ]]; then
+        CMAKE_OSX_ARCHITECTURES="x86_64;arm64"
+    elif [[ "$arch" != '' ]]; then
+        CMAKE_OSX_ARCHITECTURES="$arch"
+    fi
+    client_cmake_flags+=(-DCMAKE_OSX_ARCHITECTURES="${CMAKE_OSX_ARCHITECTURES}")
+    # build qrencode
+    (
+        cd ${TOP}
+        ./extras/scripts/build_qrencode.sh -a "$arch"
+    )
+fi
 
 if [ "${global}" = "true" ]; then
     client_cmake_flags+=(${prefix:+"-DCMAKE_INSTALL_PREFIX=$prefix"}
