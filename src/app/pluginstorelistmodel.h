dad7f30d719f92cd99b4c2a5fe1f7bc314676caa
Xavier Jouslin de Noray <xavier.jouslindenoray@savoirfairelinux.com>
2023-06-28 17:48:55 -0400

    UpdateManager: create a plugin manager for plugin store
    Change-Id: I57ebec72c1cb6e2f245af011def82f880bc9573f

--- src/app/pluginstorelistmodel.h
+++ src/app/pluginstorelistmodel.h
@@ -1,6 +1,6 @@
 /**
  * Copyright (C) 2019-2023 Savoir-faire Linux Inc.
- * Author: Aline Gondim Santos   <aline.gondimsantos@savoirfairelinux.com>
+ * Author: Xavier Jouslin de Noray   <xavier.jouslindenoray@savoirfairelinux.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,53 +15,60 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 #pragma once
 
 #include "abstractlistmodelbase.h"
 #include "pluginversionmanager.h"
 
-class LRCInstance;
+class QColor;
+class QString;
 
-class PluginListModel : public AbstractListModelBase
+#define PLUGINSTORE_ROLES \
+    X(Id) \
+    X(Title) \
+    X(IconPath) \
+    X(Background) \
+    X(Description) \
+    X(Status) \
+    X(Author)
+
+namespace PluginStoreList {
+Q_NAMESPACE
+enum Role {
+    DummyRole = Qt::UserRole + 1,
+#define X(role) role,
+    PLUGINSTORE_ROLES
+#undef X
+};
+Q_ENUM_NS(Role)
+} // namespace PluginStoreList
+
+class PluginStoreListModel : public AbstractListModelBase
 {
     Q_OBJECT
-    QML_PROPERTY(bool, filterAccount)
+
 public:
-    enum Role { PluginName = Qt::UserRole + 1, PluginId, PluginIcon, IsLoaded };
-    Q_ENUM(Role)
+    explicit PluginStoreListModel(QObject* parent = nullptr);
 
-    explicit PluginListModel(QObject* parent = nullptr);
-    ~PluginListModel();
-
-    /*
-     * QAbstractListModel override.
-     */
     int rowCount(const QModelIndex& parent = QModelIndex()) const override;
-    int columnCount(const QModelIndex& parent) const override;
     QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
-    /*
-     * Override role name as access point in qml.
-     */
     QHash<int, QByteArray> roleNames() const override;
 
-    /*
-     * This function is to reset the model when there's new account added.
-     */
     Q_INVOKABLE void reset();
-    Q_INVOKABLE void removePlugin(int index);
-    Q_INVOKABLE void pluginChanged(int index);
-    Q_INVOKABLE void addPlugin();
+
+    void addPlugin(const QVariantMap& plugin);
+    void setPlugins(const QList<QVariantMap>& plugins);
+    void removePlugin(const QString& pluginId);
+    void updatePlugin(const QVariantMap& plugin);
+    Q_INVOKABLE QColor computeAverageColorOfImage(const QString& fileUrl);
 
 Q_SIGNALS:
-    void versionCheckRequested(const QString& pluginId);
-    void setVersionStatus(const QString& pluginId, PluginStatus::Role status);
-    void autoUpdateChanged(bool state);
+    void pluginAdded(const QString& pluginId);
 
 public Q_SLOTS:
     void onVersionStatusChanged(const QString& pluginId, PluginStatus::Role status);
 
 private:
-    void filterPlugins(VectorString& list) const;
-    VectorString installedPlugins_ {};
+    using Role = PluginStoreList::Role;
+    QList<QVariantMap> plugins_;
 };
