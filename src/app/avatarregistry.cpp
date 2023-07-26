0220cb836c3c50ab4da158497b008423c592be28
Charles <charles-francis.damedey@savoirfairelinux.com>
2023-07-26 10:32:04 -0400

    chatview: update image uri for contact avatar
    Gitlab: #1267
    Change-Id: I651232cbbf68997f8111c9374486bde4bd8ceeef

--- src/app/avatarregistry.cpp
+++ src/app/avatarregistry.cpp
@@ -77,6 +77,7 @@ void
 AvatarRegistry::onProfileUpdated(const QString& uri)
 {
     auto& convInfo = lrcInstance_->getConversationFromPeerUri(uri);
+    addOrUpdateImage(uri);
     if (convInfo.uid.isEmpty())
         return;
 
