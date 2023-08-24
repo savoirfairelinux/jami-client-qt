a598c4ff9770a84709636874e59c12da33e2b2d8
lcoursodon <liam.coursodon@savoirfairelinux.com>
2023-08-24 16:27:16 -0400

    TroubleshootingSetting: Connection monitoring
    Change-Id: Idf922df701cdf2efc2d167362dca021897d8e2e5

---
+++ src/app/connectioninfolistmodel.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "abstractlistmodelbase.h"
+
+#define CONNECTONINFO_ROLES \
+    X(ConnectionDatas) \
+    X(PeerName) \
+    X(PeerId) \
+    X(DeviceId) \
+    X(Status) \
+    X(Channels) \
+    X(Count) // this is the number of connections (convenience)
+
+namespace ConnectionInfoList {
+Q_NAMESPACE
+enum Role {
+    DummyRole = Qt::UserRole + 1,
+#define X(role) role,
+    CONNECTONINFO_ROLES
+#undef X
+};
+Q_ENUM_NS(Role)
+} // namespace ConnectionInfoList
+
+class ConnectionInfoListModel : public AbstractListModelBase
+{
+public:
+    explicit ConnectionInfoListModel(LRCInstance* instance, QObject* parent = nullptr);
+
+    int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
+    QHash<int, QByteArray> roleNames() const override;
+
+    // get data from backend
+    Q_INVOKABLE void update();
+
+private:
+    using Role = ConnectionInfoList::Role;
+
+    VectorMapStringString connectionInfoList_;
+
+    // Transformed data.
+    // peerIds_ will be used to generate to granularize the data changes.
+    // Example (NOTE: Avatar and PeerName are synthesized from peerData_):
+    // Also, we want to remove the peerId from peerData_.
+    // peerData_ = {
+    //     "peerId1": {
+    //         "deviceId1": {
+    //             "status": "online",
+    //             "channels": ["channel1", "channel2"]
+    //         },
+    //         "deviceId2": {
+    //             "status": "offline",
+    //             "channels": ["channel3"]
+    //         }
+    //     },
+    //     "peerId2": {
+    //         "deviceId3": {
+    //             "status": "online",
+    //             "channels": ["channel4"]
+    //         }
+    //     }
+    // }
+    QVector<QString> peerIds_;
+    QMap<QString, QMap<QString, QMap<QString, QVariant>>> peerData_;
+    void aggregateData();
+    void resetData();
+};
