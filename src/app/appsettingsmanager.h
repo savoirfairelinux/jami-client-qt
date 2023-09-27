7a34826b6ce03654b24879bb83dd7579b7326739
lcoursodon <liam.coursodon@savoirfairelinux.com>
2023-09-27 11:26:18 -0400

    Donation campaign: add banner in smartlist
    GitLab: #1334
    Change-Id: I53b23eabab47389b9bea50f54afac28d41741b0b

--- src/app/appsettingsmanager.h
+++ src/app/appsettingsmanager.h
@@ -26,6 +26,7 @@
 #include <QObject>
 #include <QString>
 #include <QStandardPaths>
+#include <QDateTime>
 #include <QWindow> // for QWindow::AutomaticVisibility
 
 #include <QTranslator>
@@ -62,8 +63,8 @@ extern const QString defaultDownloadPath;
     X(FlipSelf, true) \
     X(ShowMardownOption, false) \
     X(ChatViewEnterIsNewLine, false) \
-    X(ShowSendOption, false)
-
+    X(ShowSendOption, false) \
+    X(DonateVisibleDate,  "2999-02-01 05:00")
 /*
  * A class to expose settings keys in both c++ and QML.
  * Note: this is using a non-constructable class instead of a
