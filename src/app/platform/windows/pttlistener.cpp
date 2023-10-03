e32d83e01b99d31c98fb515bb5fe60c558a26ce6
cberthet <capucine.berthet@savoirfairelinux.com>
2023-10-03 15:06:50 -0400

    Push To Talk: add global and local ptt
    It works with a Pimpl which the right pttlistener.cpp depending on the platform you are on (macOs, windows or X11). It is a global PTT which listen to key events during calls. If the global PTT is not supported, a local PTT is set.
    
    GitLab: #1402
    Change-Id: I8399800966c737bb8e8a656ecbb6af7ac7cdde8c

---
+++ src/app/platform/windows/pttlistener.cpp
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2020-2023 Savoir-faire Linux Inc.
+ * Author: Capucine Berthet <capucine.berthet@savoirfairelinux.com>
+ * Author: Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
+ * Author: Sébastien Blin <sebastien.blin@savoirfairelinux.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+#include "pttlistener.h"
+
+#include <QCoreApplication>
+#include <QVariant>
+
+#include <windows.h>
+
+class
+{
+    Q_OBJECT
+public:
+    Impl(PTTListener* parent)
+        : QObject(parent)
+    {
+        qApp->setProperty("PTTListener", QVariant::fromValue(parent));
+    }
+
+    ~Impl()
+    {
+        stopListening();
+    };
+
+    void startListening()
+    {
+        keyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, GlobalKeyboardProc, NULL, 0);
+    }
+
+    void stopListening()
+    {
+        UnhookWindowsHookEx(keyboardHook);
+    }
+
+    static LRESULT CALLBACK GlobalKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
+    {
+        auto* pThis = qApp->property("PTTListener").value<PTTListener*>();
+        if (pThis == nullptr) {
+            qWarning() << "PTTListener not found";
+            return {};
+        }
+        auto* keyboardHook = pThis->pimpl_->keyboardHook;
+        if (nCode == HC_ACTION) {
+            if (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN) {
+                Q_EMIT pThis->PTTKeyPressed();
+            } else if (wParam == WM_KEYUP || wParam == WM_SYSKEYUP) {
+                Q_EMIT pThis->PTTKeyReleased();
+            }
+        }
+
+        return CallNextHookEx(keyboardHook, nCode, wParam, lParam);
+    }
+
+    HHOOK keyboardHook;
+};
+
+PTTListener::PTTListener(QObject* parent)
+    : QObject(parent)
+    , pimpl_(std::make_unique<Impl>(this))
+{}
+
+PTTListener::~PTTListener() = default;
+
+void
+PTTListener::startListening()
+{
+    pimpl_->startListening();
+}
+
+void
+PTTListener::stopListening()
+{
+    pimpl_->stopListening();
+}
+
+#include "pttlistener.moc"
