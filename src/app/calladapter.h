617f064780f7471251ee8c1a97db23df56f3eeb6
cberthet <capucine.berthet@savoirfairelinux.com>
2023-10-03 15:06:50 -0400

    Push To Talk: add global and local ptt
    It works with a Pimpl which the right pttlistener.cpp depending on the platform you are on (macOs, windows or X11). It is a global PTT which listen to key events during calls. If the global PTT is not supported, a local PTT is set.
    
    GitLab: #1402
    Change-Id: I8399800966c737bb8e8a656ecbb6af7ac7cdde8c

--- src/app/calladapter.h
+++ src/app/calladapter.h
@@ -24,6 +24,7 @@
 #include "qmladapterbase.h"
 #include "screensaver.h"
 #include "calloverlaymodel.h"
+#include "pttlistener.h"
 
 #include <QObject>
 #include <QString>
@@ -46,7 +47,7 @@ public:
     Q_ENUM(MuteStates)
 
     explicit CallAdapter(SystemTray* systemTray, LRCInstance* instance, QObject* parent = nullptr);
-    ~CallAdapter() = default;
+    ~CallAdapter();
 
 public:
     Q_INVOKABLE void startTimerInformation();
@@ -76,6 +77,9 @@ public:
     Q_INVOKABLE void holdThisCallToggle();
     Q_INVOKABLE void recordThisCallToggle();
     Q_INVOKABLE void muteAudioToggle();
+    Q_INVOKABLE bool isMuted();
+    Q_INVOKABLE void connectPtt();
+    Q_INVOKABLE void disconnectPtt();
     Q_INVOKABLE void muteCameraToggle();
     Q_INVOKABLE bool isRecordingThisCall();
     Q_INVOKABLE void muteParticipant(const QString& accountUri,
@@ -121,6 +125,10 @@ private:
     SystemTray* systemTray_;
     QScopedPointer<CallOverlayModel> overlayModel_;
     VectorString currentConfSubcalls_;
-
     std::unique_ptr<CallInformationListModel> callInformationListModel_;
+
+#ifdef HAVE_GLOBAL_PTT
+    PTTListener listener_ = new PTTListener(this);
+    bool isMicrophoneMuted_ = true;
+#endif
 };
