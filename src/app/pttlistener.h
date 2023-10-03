69348b56c1df52db14554788b8a54a68fd7aa3a3
cberthet <capucine.berthet@savoirfairelinux.com>
2023-10-03 15:06:50 -0400

    Push-To-Talk : add global and local ptt
    It works with a Pimpl which the right pttlistener.cpp depending
    on the platform you are on (macOs, windows or X11). It is a
    global PTT which listen to key events during calls. If the
    global PTT is not supported, a local PTT is set.
    
    GitLab: #1402
    Change-Id: I8399800966c737bb8e8a656ecbb6af7ac7cdde8c

---
+++ src/app/pttlistener.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "systemtray.h"
+#include "appsettingsmanager.h"
+
+#include <QObject>
+#include <QThread>
+#include <QKeyEvent>
+
+class PTTListener : public QObject
+{
+    Q_OBJECT
+
+public:
+    Q_INVOKABLE Qt::Key getCurrentKey()
+    {
+        int keyInt = settingsManager_->getValue(Settings::Key::pttKey).toInt();
+        Qt::Key key = static_cast<Qt::Key>(keyInt);
+        return key;
+    }
+
+    Q_INVOKABLE QString keyToString(Qt::Key key)
+    {
+        return QKeySequence(key).toString();
+    }
+    Q_INVOKABLE void setPttKey(Qt::Key key)
+    {
+        settingsManager_->setValue(Settings::Key::pttKey, key);
+    }
+    Q_INVOKABLE bool getPttState()
+    {
+        return settingsManager_->getValue(Settings::Key::EnablePtt).toBool();
+    }
+
+    PTTListener(AppSettingsManager* settingsManager, QObject* parent = nullptr);
+    ~PTTListener();
+
+Q_SIGNALS:
+    void pttKeyPressed();
+    void pttKeyReleased();
+
+#ifdef HAVE_GLOBAL_PTT
+public Q_SLOTS:
+    void startListening();
+    void stopListening();
+#endif
+
+private:
+    class Impl;
+    std::unique_ptr<Impl> pimpl_;
+    AppSettingsManager* settingsManager_;
+};
