5371dac88246cadd6e9b8d0dc73d5ea550b60a36
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2024-02-16 09:39:03 -0500

    misc: use BUILD_TESTING like the daemon and update instructions
    Change-Id: I5e92e47ada4c4225c68065179245d96723397575

--- extras/scripts/build-windows.py
+++ extras/scripts/build-windows.py
@@ -280,7 +280,7 @@ def build(config_str, qt_dir, tests):
         "-DCMAKE_INSTALL_PREFIX=" + os.getcwd(),
         "-DCMAKE_SYSTEM_VERSION=" + WIN_SDK_VERSION,
         "-DCMAKE_BUILD_TYPE=" + "Release",
-        "-DENABLE_TESTS=" + str(tests).lower(),
+        "-DBUILD_TESTING=" + str(tests).lower(),
         "-DBETA=" + str((0, 1)[config_str == "Beta"]),
     ]
 
