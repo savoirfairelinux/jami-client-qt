0a17f6f8dc21c2b864353d90a4057aee992afa51
Jerome Lamy <jerome.lamy@savoirfairelinux.com>
2024-03-14 10:11:55 -0400

    hunspell: Get dictionary path
    Implement API to return the hardcoded dictionary path from hunspell linux
    install.
    
    Change-Id: Ib36908cf3f246f49acce24daf6b6355ea7cdab89

--- src/app/appsettingsmanager.h
+++ src/app/appsettingsmanager.h
@@ -146,6 +146,7 @@ public:
     Q_INVOKABLE QVariant getDefault(const Settings::Key key) const;
 
     QString getLanguage();
+    QString getDictionaryPath();
 
     void loadTranslations();
     void loadHistory();
