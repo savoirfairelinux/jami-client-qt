26b8bcaf6f79cf55d30956dc51ee8dcef41ca416
Christophe Imbaud <christophe.imbaud@savoirfairelinux.com>
2024-04-11 13:41:45 -0400

    packaging: Add Ubuntu 24.04 LTS
    Change-Id: I3dc28375adfc9fa20d162f46d8c46f54a28dca7e

--- extras/packaging/gnu-linux/scripts/build-package-debian.sh
+++ extras/packaging/gnu-linux/scripts/build-package-debian.sh
@@ -94,6 +94,21 @@ ${QT_MAJOR}.${QT_MINOR}/${qt_version}/single
         DEBEMAIL="The Jami project <jami@gnu.org>" dch --release \
                 --distribution "unstable" debian/changelog
 
+        # HACK: For now on ubuntu 24.04 there is no python3.10 package
+        # So create a conda environment to install the required packages
+        CONDA=/root/miniconda3/bin/conda
+        if command -v ${CONDA} &> /dev/null; then
+            ${CONDA} create -n chromium python=3.10 -y
+            echo $(cat ~/.bashrc)
+            ${CONDA} init
+            su -
+            export PATH="/root/miniconda3/bin:$PATH"
+            conda activate chromium
+            python -m pip install html5lib
+            python -m pip install six
+        fi
+
+
         # Build and package Qt.
         dpkg-buildpackage -uc -us -d ${DPKG_BUILD_OPTIONS}
 
