385824f61a4e4aa21e63250dc7684b0019b41c80
François-Simon Fauteux-Chapleau <francois-simon.fauteux-chapleau@savoirfairelinux.com>
2024-04-16 13:53:26 -0400

    snap: build PipeWire from source
    This is necessary because the Docker image we use for Snap is based on
    Ubuntu 20.04, which means that the version of PipeWire available via apt
    is too old.
    
    Change-Id: Id039ec446f4b3f0e89b9ec27f37f81cfd6b3587e

---
+++ extras/packaging/gnu-linux/scripts/install-pipewire-from-source.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+cd /tmp
+
+# Install PipeWire build dependencies
+apt-get install --yes gcc git libasound2-dev libdbus-1-dev libglib2.0-dev ninja-build pkg-config
+
+# Get a version of Meson that's recent enough to build PipeWire 1.0.5 (the one available via apt is too old)
+wget -q https://github.com/mesonbuild/meson/releases/download/0.61.1/meson-0.61.1.tar.gz
+echo "feb2cefb325b437dbf36146df7c6b87688ddff0b0205caa31dc64055c6da410c  meson-0.61.1.tar.gz" | sha256sum --check
+tar xzf meson-0.61.1.tar.gz
+
+# Build and install PipeWire 1.0.5
+wget -q https://gitlab.freedesktop.org/pipewire/pipewire/-/archive/1.0.5/pipewire-1.0.5.tar.gz
+echo "c5a5de26d684a1a84060ad7b6131654fb2835e03fccad85059be92f8e3ffe993  pipewire-1.0.5.tar.gz" | sha256sum --check
+tar xzf pipewire-1.0.5.tar.gz
+cd pipewire-1.0.5
+python3 ../meson-0.61.1/meson.py setup builddir -Dsession-managers=media-session -Dalsa=disabled -Dprefix=/usr/local
+python3 ../meson-0.61.1/meson.py compile -C builddir
+python3 ../meson-0.61.1/meson.py install -C builddir
+
+# Cleanup
+rm -rf meson-0.61.1  meson-0.61.1.tar.gz  pipewire-1.0.5  pipewire-1.0.5.tar.gz
