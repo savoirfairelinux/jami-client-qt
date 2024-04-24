84f1ff8561ccd56585e8a025ebcec894118f99a7
jerome Lamy <jerome.lamy@savoirfairelinux.com>
2024-04-24 11:17:38 -0400

    hunspell: implement update dictionary path
    Add a function to update the dictionary path on the fly. Will be use
    when the possibility to change the spelling language will be
    implemented.
    
    Change-Id: I4423d89acf0fc53008cc5f725990c73174f4cdf5

--- src/app/spellchecker.cpp
+++ src/app/spellchecker.cpp
@@ -30,28 +30,7 @@
 
 SpellChecker::SpellChecker(const QString& dictionaryPath)
 {
-    QString dictFile = dictionaryPath + ".dic";
-    QString affixFile = dictionaryPath + ".aff";
-    QByteArray dictFilePathBA = dictFile.toLocal8Bit();
-    QByteArray affixFilePathBA = affixFile.toLocal8Bit();
-    _hunspell = std::make_shared<Hunspell>(affixFilePathBA.constData(), dictFilePathBA.constData());
-
-    // detect encoding analyzing the SET option in the affix file
-    _encoding = "ISO8859-1";
-    QFile _affixFile(affixFile);
-    if (_affixFile.open(QIODevice::ReadOnly)) {
-        QTextStream stream(&_affixFile);
-        QRegExp enc_detector("^\\s*SET\\s+([A-Z0-9\\-]+)\\s*", Qt::CaseInsensitive);
-        for (QString line = stream.readLine(); !line.isEmpty(); line = stream.readLine()) {
-            if (enc_detector.indexIn(line) > -1) {
-                _encoding = enc_detector.cap(1);
-                break;
-            }
-        }
-        _affixFile.close();
-    }
-
-    _codec = QTextCodec::codecForName(this->_encoding.toLatin1().constData());
+    replaceDictionary(dictionaryPath);
 }
 
 bool
@@ -85,4 +64,34 @@ void
 SpellChecker::put_word(const QString& word)
 {
     _hunspell->add(_codec->fromUnicode(word).constData());
+}
+
+void
+SpellChecker::replaceDictionary(const QString& dictionaryPath)
+{
+    QString dictFile = dictionaryPath + ".dic";
+    QString affixFile = dictionaryPath + ".aff";
+    QByteArray dictFilePathBA = dictFile.toLocal8Bit();
+    QByteArray affixFilePathBA = affixFile.toLocal8Bit();
+    if (_hunspell) {
+        _hunspell.reset();
+    }
+    _hunspell = std::make_shared<Hunspell>(affixFilePathBA.constData(), dictFilePathBA.constData());
+
+    // detect encoding analyzing the SET option in the affix file
+    _encoding = "ISO8859-1";
+    QFile _affixFile(affixFile);
+    if (_affixFile.open(QIODevice::ReadOnly)) {
+        QTextStream stream(&_affixFile);
+        QRegExp enc_detector("^\\s*SET\\s+([A-Z0-9\\-]+)\\s*", Qt::CaseInsensitive);
+        for (QString line = stream.readLine(); !line.isEmpty(); line = stream.readLine()) {
+            if (enc_detector.indexIn(line) > -1) {
+                _encoding = enc_detector.cap(1);
+                break;
+            }
+        }
+        _affixFile.close();
+    }
+
+    _codec = QTextCodec::codecForName(this->_encoding.toLatin1().constData());
 }
