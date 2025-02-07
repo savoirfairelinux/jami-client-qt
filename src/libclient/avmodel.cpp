05f5963b75064e88d11cb196f68c3e68c7ded583
ovari123 <ovari123@zoho.com>
2025-02-06 21:47:26 -0400

    copyright: happy new year!
    Change-Id: I46a0d1297c2e60ca43dc5524fd6e8a192bc45139

--- src/libclient/avmodel.cpp
+++ src/libclient/avmodel.cpp
@@ -1,7 +1,5 @@
 /*
- *  Copyright (C) 2018-2024 Savoir-faire Linux Inc.
- *  Author: Hugo Lefeuvre <hugo.lefeuvre@savoirfairelinux.com>
- *  Author: Sébastien Blin <sebastien.blin@savoirfairelinux.com>
+ *  Copyright (C) 2018-2025 Savoir-faire Linux Inc.
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
