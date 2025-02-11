5b44c981eb98b96649098a92f28e414b4491f249
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2025-02-11 15:45:07 -0500

    Account: link device
    Change-Id: I3d3486380e695ea44c199dbe0a0448f724b4d2db

--- src/libclient/callbackshandler.h
+++ src/libclient/callbackshandler.h
@@ -247,6 +247,19 @@ Q_SIGNALS:
     void deviceAuthStateChanged(const QString& accountId, int state, const MapStringString& details);
 
     /**
+     * Add device state has changed
+     * @param accountId
+     * @param operationId
+     * @param state
+     * @param details map
+     */
+
+    void addDeviceStateChanged(const QString& accountId,
+                               uint32_t operationId,
+                               int state,
+                               const MapStringString& details);
+
+    /**
      * Name registration has ended
      * @param accountId
      * @param status
@@ -588,6 +601,18 @@ private Q_SLOTS:
     void slotDeviceAuthStateChanged(const QString& accountId,
                                     int state,
                                     const MapStringString& details);
+
+    /**
+     * Add device state has changed
+     * @param accountId
+     * @param operationId
+     * @param state
+     * @param details map
+     */
+    void slotAddDeviceStateChanged(const QString& accountId,
+                                   uint32_t operationId,
+                                   int state,
+                                   const MapStringString& details);
 
     /**
      * Emit nameRegistrationEnded
