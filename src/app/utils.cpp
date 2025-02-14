894ff844b7d22d16a5da3493b0eca9379189e790
ovari123 <ovari123@zoho.com>
2025-02-13 21:15:59 -0500

    src/app/utils.cpp: cleanup
    Remove consecutive whitespaces in string.
    
    https://app.transifex.com/savoirfairelinux/search/?q=source_language%3Aen+target_language%3Ahu+source_text%3A%27%5BThis+is+a+list+of+people+who+have+made+a+significant+investment+of+time%2C++++++++with+useful+results%2C+into+Jami.+Any+such+contributors+who+want+to+be+added+to+the+list+++++++++should+contact+us.%5D%27
    
    Change-Id: Id7ef7dac3a8dfafa50c69dc3182b8654b1695b72

--- src/app/utils.cpp
+++ src/app/utils.cpp
@@ -618,10 +618,12 @@ Utils::getProjectCredits()
     QTextStream in(&projectCreditsFile);
     return in.readAll().arg(
         QObject::tr("We would like to thank our contributors, whose efforts over many years have made this software what it is."),
-        QObject::tr("Developers"), QObject::tr("Media"), QObject::tr("Community Management"), QObject::tr("Special thanks to"),
-        QObject::tr("This is a list of people who have made a significant investment of time,\
-        with useful results, into Jami. Any such contributors who want to be added to the list \
-        should contact us."));
+        QObject::tr("Developers"),
+        QObject::tr("Media"),
+        QObject::tr("Community Management"),
+        QObject::tr("Special thanks to"),
+        QObject::tr("This is a list of people who have made a significant investment of time, with useful results, into Jami. Any such contributors who want to be added to the list should contact us.")
+      );
 }
 
 inline QString
