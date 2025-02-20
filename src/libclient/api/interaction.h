a060d2e247dfaa0ab0e69f1893bf6bea6a53dacd
pmagnier-slimani <page.magnier-slimani@savoirfairelinux.com>
2025-02-20 11:34:06 -0500

    wip
    Change-Id: I87c53666057ecb168ea186f6dd93a24791e470e0

--- src/libclient/api/interaction.h
+++ src/libclient/api/interaction.h
@@ -43,6 +43,7 @@ enum class Type {
     REACTION,
     VOTE,
     UPDATE_PROFILE,
+    MEMBER,
     COUNT__
 };
 Q_ENUM_NS(Type)
@@ -111,15 +112,7 @@ to_type(const QString& type)
         return interaction::Type::INVALID;
 }
 
-enum class Status {
-    INVALID,
-    UNKNOWN,
-    SENDING,
-    FAILURE,
-    SUCCESS,
-    DISPLAYED,
-    COUNT__
-};
+enum class Status { INVALID, UNKNOWN, SENDING, FAILURE, SUCCESS, DISPLAYED, COUNT__ };
 Q_ENUM_NS(Status)
 
 enum class TransferStatus {
