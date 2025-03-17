a655219f41808add90be99fe2be33d07b76f2747
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2025-03-17 13:26:33 -0400

    build: update macOS dependencies
    - adds natpmp
    - removes freetype
    
    Change-Id: Iba6c311dff4fb175efbe16cc1f82e51355f5d25e

--- extras/scripts/build_daemon_macos.sh
+++ extras/scripts/build_daemon_macos.sh
@@ -52,7 +52,7 @@ for ARCH in "${ARCHS[@]}"; do
     # force to build every contrib
     for dir in "$DAEMON"/contrib/src/*/; do
       PKG=$(basename -- "$dir")
-      if [ "$PKG" != "sdbus-cpp" ] && [ "$PKG" != "natpmp" ] &&
+      if [ "$PKG" != "sdbus-cpp" ] && [ "$PKG" != "freetype" ] &&
         [ "$PKG" != "portaudio" ] && [ "$PKG" != "pthreads" ] &&
         [ "$PKG" != "lttng-ust" ] && [ "$PKG" != "openssl" ] &&
         [ "$PKG" != "media-sdk" ] && [ "$PKG" != "jack" ] &&
