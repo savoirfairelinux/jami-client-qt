10621704f94771b805f2af99634cb3856a7f1ba8
pmagnier-slimani <page.magnier-slimani@savoirfairelinux.com>
2025-05-12 10:46:10 -0400

    spellcheck: windows and macos
    Implement the hunspell spellchecker for Windows and MacOS. It also changes
    the base implementation for Linux. The system dictionaries (if any) are
    aggregated with those installed from the LibreOffice repository via Jami's
    dictionary managemnet interface.
    
    This commit implements a major refactoring of the spellcheck system to
    improve UI responsiveness and user experience:
    
    Core Changes:
    - Used QAbstractListModel to represent the list of dictionaries
    - Added new QML components:
      - DictionaryInstallView.qml
      - ManageDictionariesDialog.qml
      - SpellCheckLanguageComboBox.qml
    - Updated property names for clarity
    - Fixed a bug in the settings combo box custom component that caused
      out-of-range errors for filtered models
    
    GitLab: #1997
    Change-Id: Ibd0879f957f27f4c7c5720762ace553ca84e2bc3

--- src/app/qmlregister.h
+++ src/app/qmlregister.h
@@ -32,7 +32,6 @@
 class SystemTray;
 class LRCInstance;
 class AppSettingsManager;
-class SpellCheckDictionaryManager;
 class PreviewEngine;
 class ScreenInfo;
 class MainApplication;
@@ -62,7 +61,6 @@ void registerTypes(QQmlEngine* engine,
                    LRCInstance* lrcInstance,
                    SystemTray* systemTray,
                    AppSettingsManager* appSettingsManager,
-                   SpellCheckDictionaryManager* spellCheckDictionaryManager,
                    ConnectivityMonitor* connectivityMonitor,
                    PreviewEngine* previewEngine,
                    ScreenInfo* screenInfo,
---
+++ src/app/spellcheckadapter.h
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2025 Savoir-faire Linux Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "spellchecker.h"
+#include "qtutils.h"
+
+#include <QObject>
+#include <QQmlEngine>   // QML registration
+#include <QApplication> // QML registration
+
+class SpellCheckDictionaryListModel;
+class AppSettingsManager;
+
+class SpellCheckAdapter final : public QObject
+{
+    Q_OBJECT
+    QML_SINGLETON
+
+    QML_RO_PROPERTY(int, installedDictionaryCount)
+
+public:
+    static SpellCheckAdapter* create(QQmlEngine* engine, QJSEngine*)
+    {
+        return new SpellCheckAdapter(
+            qApp->property("SpellCheckDictionaryListModel").value<SpellCheckDictionaryListModel*>(),
+            qApp->property("AppSettingsManager").value<AppSettingsManager*>(),
+            engine);
+    }
+
+    explicit SpellCheckAdapter(SpellCheckDictionaryListModel* dictionaryListModel,
+                               AppSettingsManager* settingsManager,
+                               QObject* parent = nullptr);
+    ~SpellCheckAdapter() = default;
+
+    Q_INVOKABLE QVariant getDictionaryListModel() const;
+    Q_INVOKABLE QVariantMap getInstalledDictionaries() const;
+
+    Q_INVOKABLE void installDictionary(const QString& locale);
+    Q_INVOKABLE void uninstallDictionary(const QString& locale);
+
+    Q_INVOKABLE QVariantList spellSuggestionsRequest(const QString& word);
+    Q_INVOKABLE bool spell(const QString& word);
+    Q_INVOKABLE QVariantList findWords(const QString& text);
+
+public Q_SLOTS:
+    Q_INVOKABLE void setDictionary(const QString& locale);
+
+Q_SIGNALS:
+    void dictionaryChanged();
+    void downloadFailed(const QString& locale);
+
+private:
+    SpellChecker spellChecker_;
+    SpellCheckDictionaryListModel* dictionaryListModel_ {nullptr};
+    AppSettingsManager* settingsManager_ {nullptr};
+};
