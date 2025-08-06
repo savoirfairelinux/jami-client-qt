f1762291a70daf51fbae37fd3aa2ccbfbe215412
François-Simon Fauteux-Chapleau <francois-simon.fauteux-chapleau@savoirfairelinux.com>
2025-08-06 10:19:44 -0400

    snap: use core22 as base
    Use core22 (Ubuntu 22.04) instead of core20 (Ubuntu 20.04) as the base
    for the snap build. This will allow us to stop building libqt-jami for
    Ubuntu 20.04, which has reached its EOL.
    
    The changes made in snapcraft.yaml are based on following guide:
    https://documentation.ubuntu.com/snapcraft/latest/how-to/change-bases/change-from-core20-to-core22/
    
    GitLab: #1944
    Change-Id: Ib1af9c28a1ca9d0c3b8e693c5b2e50fb6cbead64

--- extras/packaging/gnu-linux/scripts/install-pipewire-from-source.sh
+++ extras/packaging/gnu-linux/scripts/install-pipewire-from-source.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# The purpose of this script is to build PipeWire from source in a snap based on core20 / Ubuntu 20.04
+# The purpose of this script is to build PipeWire from source in a snap based on core22 / Ubuntu 22.04
 # It must be called in the "override-build" section of the relevant part in snapcraft.yaml
 
 set -e
@@ -8,28 +8,23 @@ set -e
 OLD_WD=$(pwd)
 cd /tmp
 
-# Get a version of Meson that's recent enough to build PipeWire 1.0.5 (the one available via apt is too old)
-wget -q https://github.com/mesonbuild/meson/releases/download/0.61.1/meson-0.61.1.tar.gz
-echo "feb2cefb325b437dbf36146df7c6b87688ddff0b0205caa31dc64055c6da410c  meson-0.61.1.tar.gz" | sha256sum --check
-tar xzf meson-0.61.1.tar.gz
-
 # Build PipeWire 1.0.5 and install it in the /usr directory of the build environment
 wget -q https://gitlab.freedesktop.org/pipewire/pipewire/-/archive/1.0.5/pipewire-1.0.5.tar.gz
 echo "c5a5de26d684a1a84060ad7b6131654fb2835e03fccad85059be92f8e3ffe993  pipewire-1.0.5.tar.gz" | sha256sum --check
 tar xzf pipewire-1.0.5.tar.gz
 cd pipewire-1.0.5
-../meson-0.61.1/meson.py setup builddir -Dsession-managers=media-session -Dalsa=disabled -Dprefix=/usr
-../meson-0.61.1/meson.py compile -C builddir
-../meson-0.61.1/meson.py install -C builddir
+meson setup builddir -Dsession-managers=media-session -Dalsa=disabled -Dprefix=/usr
+meson compile -C builddir
+meson install -C builddir
 
 # The files installed by the previous command are only for the "Build" step of the snap
 # creation process (https://snapcraft.io/docs/how-snapcraft-builds). In order to ensure
 # that PipeWire is installed in the final snap archive, we also need to copy all the
 # required files under the $SNAPCRAFT_PART_INSTALL directory.
-../meson-0.61.1/meson.py configure builddir -Dprefix=$SNAPCRAFT_PART_INSTALL/usr/
-../meson-0.61.1/meson.py install -C builddir
+meson configure builddir -Dprefix=$SNAPCRAFT_PART_INSTALL/usr/
+meson install -C builddir
 
 # Cleanup
 cd /tmp
-rm -rf meson-0.61.1  meson-0.61.1.tar.gz  pipewire-1.0.5  pipewire-1.0.5.tar.gz
+rm -rf pipewire-1.0.5  pipewire-1.0.5.tar.gz
 cd $OLD_WD
