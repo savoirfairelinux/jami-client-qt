9a6dfaae3bce59a7938d5463d173eb596334d751
ovari123 <ovari123@zoho.com>
2025-09-17 23:48:09 -0400

    src/libclient/api/interaction.h: translate call duration
    GitLab: #1979
    Change-Id: I1f6bd06ef3c72cd395f45d8be40fa48f8df4022a

--- src/libclient/api/interaction.h
+++ src/libclient/api/interaction.h
@@ -14,6 +14,7 @@
  *   You should have received a copy of the GNU General Public License      *
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
  ***************************************************************************/
+
 #pragma once
 
 #include <QString>
@@ -308,31 +309,31 @@ getFormattedCallDuration(const std::time_t duration)
     if (duration == 0)
         return {};
 
-    int minutes = duration / 60;
     int seconds = duration % 60;
+    int minutes = duration / 60;
     int hours = minutes / 60;
     int days = hours / 24;
 
     if (days > 0) {
         hours = hours % 24;
         minutes = minutes % 60;
-        return QString("%1d %2h %3m %4s")
+        return QString::tr("%1d %2h %3m %4s")
             .arg(days)
             .arg(hours)
             .arg(minutes)
             .arg(seconds);
     } else if (hours > 0) {
         minutes = minutes % 60;
-        return QString("%1h %2m %3s")
+        return QString::tr("%1h %2m %3s")
             .arg(hours)
             .arg(minutes)
             .arg(seconds);
     } else if (minutes > 0) {
-        return QString("%1m %2s")
+        return QString::tr("%1m %2s")
             .arg(minutes)
             .arg(seconds);
     } else {
-        return QString("%1s")
+        return QString::tr("%1s")
             .arg(seconds);
     }
 }
