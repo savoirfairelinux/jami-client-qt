ddb5c16b5bef3aa62730639766fbfc32441ac429
ovari123 <ovari123@zoho.com>
2025-11-04 22:29:37 -0400

    conversationmodel.cpp: update unhold to resume
    GitLab: https://git.jami.net/savoirfairelinux/jami-daemon/-/issues/1154
    Change-Id: I390d5d1cd8185c66ff022c452c2541da8e068077

--- src/libclient/conversationmodel.cpp
+++ src/libclient/conversationmodel.cpp
@@ -1,19 +1,20 @@
-/****************************************************************************
- *   Copyright (C) 2017-2025 Savoir-faire Linux Inc.                        *
- *                                                                          *
- *   This library is free software; you can redistribute it and/or          *
- *   modify it under the terms of the GNU Lesser General Public             *
- *   License as published by the Free Software Foundation; either           *
- *   version 2.1 of the License, or (at your option) any later version.     *
- *                                                                          *
- *   This library is distributed in the hope that it will be useful,        *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      *
- *   Lesser General Public License for more details.                        *
- *                                                                          *
- *   You should have received a copy of the GNU General Public License      *
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
- ***************************************************************************/
+/*
+ * Copyright (C) 2017-2025 Savoir-faire Linux Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "api/conversationmodel.h"
 
 // LRC
@@ -740,7 +741,7 @@ ConversationModel::selectConversation(const QString& uid) const
         if (!conversation.confId.isEmpty() && owner.confProperties.isRendezVous) {
             // If we are on a rendez vous account and we select the conversation,
             // attach to the call.
-            CallManager::instance().unholdConference(owner.id, conversation.confId);
+            CallManager::instance().resumeConference(owner.id, conversation.confId);
         }
 
         if (not callEnded and not conversation.confId.isEmpty()) {
