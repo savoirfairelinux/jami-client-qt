40c1ecc94bd17afcf152db34c216544a4211d104
Adrien Beraud <adrien.beraud@savoirfairelinux.com>
2025-11-13 14:53:21 -0500

    conversation model: carry mode from invitation
    Change-Id: Ie4c92368fdc756044eafe9b247b983424beca0cb

--- src/libclient/conversationmodel.cpp
+++ src/libclient/conversationmodel.cpp
@@ -2633,6 +2633,7 @@ ConversationModelPimpl::addConversationRequest(const MapStringString& convReques
 
     auto peerUri = convRequest["from"];
     auto mode = conversation::to_mode(convRequest["mode"].toInt());
+
     QString callId, confId;
     const MapStringString& details = ConfigurationManager::instance().conversationInfos(linked.owner.id, convId);
     conversation::Info conversation(convId, &linked.owner);
@@ -2645,6 +2646,7 @@ ConversationModelPimpl::addConversationRequest(const MapStringString& convReques
 
     MapStringString messageMap = {
         {"type", "initial"},
+        {"mode", QString::number(static_cast<int>(mode))},
         {"author", peerUri},
         {"timestamp", convRequest["received"]},
         {"linearizedParent", ""},
