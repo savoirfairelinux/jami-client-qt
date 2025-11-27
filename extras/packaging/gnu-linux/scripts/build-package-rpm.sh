53a41be7840d76adb2039990e4f9d221e04dbf30
François-Simon Fauteux-Chapleau <francois-simon.fauteux-chapleau@savoirfairelinux.com>
2025-11-27 15:22:22 -0500

    packaging: remove Fedora 41 (EOL)
    Change-Id: Ie19b09a463e725d63c600a02ce21989404b99076

--- extras/packaging/gnu-linux/scripts/build-package-rpm.sh
+++ extras/packaging/gnu-linux/scripts/build-package-rpm.sh
@@ -105,8 +105,6 @@ if [ ! -f "${RPM_PATH}" ]; then
         # Cache the built Qt RPM package.
         if [[ "${DISTRIBUTION:0:4}" == "rhel" ]]; then
             cp /root/rpmbuild/RPMS/x86_64/jami-libqt-$QT_MAJOR_MINOR_PATCH-*.el8.x86_64.rpm "${RPM_PATH}"
-        elif [[ "${DISTRIBUTION}" == "fedora_41" ]]; then
-            cp /root/rpmbuild/RPMS/x86_64/jami-libqt-$QT_MAJOR_MINOR_PATCH-*.fc41.x86_64.rpm "${RPM_PATH}"
         elif [[ "${DISTRIBUTION}" == "fedora_42" ]]; then
             cp /root/rpmbuild/RPMS/x86_64/jami-libqt-$QT_MAJOR_MINOR_PATCH-*.fc42.x86_64.rpm "${RPM_PATH}"
         elif [[ "${DISTRIBUTION}" == "fedora_43" ]]; then
