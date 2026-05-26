5048a0f1b28a327d4ac8117d21063df919af9e4d
François-Simon Fauteux-Chapleau <francois-simon.fauteux-chapleau@savoirfairelinux.com>
2026-05-26 14:54:42 -0400

    qt: update to 6.10.3
    Change-Id: I45a67bf76726971723a9ff81e68058d87300064c

--- extras/packaging/gnu-linux/scripts/install-ffmpeg-for-qt.sh
+++ extras/packaging/gnu-linux/scripts/install-ffmpeg-for-qt.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # Building Qt Multimedia requires the FFmpeg headers and libraries to be installed:
-#     https://doc.qt.io/qt-6.8/qtmultimedia-building-from-source.html
-# For Qt 6.8.3, the recommended FFmpeg version is 7.1:
-#     https://doc.qt.io/qt-6.8/qtmultimedia-index.html#target-platform-and-backend-notes
+#     https://doc.qt.io/qt-6.10/qtmultimedia-building-from-source.html
+# For Qt 6.10.3, the recommended FFmpeg version is 7.1.3:
+#     https://doc.qt.io/qt-6.10/qtmultimedia-index.html#target-platform-and-backend-notes
 # This script is based on the instructions at:
-#     https://doc.qt.io/qt-6.8/qtmultimedia-building-ffmpeg-linux.html
+#     https://doc.qt.io/qt-6.10/qtmultimedia-building-ffmpeg-linux.html
 
 set -e
 
@@ -17,7 +17,7 @@ cd nv-codec-headers
 make -j install
 
 cd /tmp
-git clone --branch n7.1.1 https://git.ffmpeg.org/ffmpeg.git ffmpeg
+git clone --branch n7.1.3 https://git.ffmpeg.org/ffmpeg.git ffmpeg
 cd ffmpeg
 mkdir build
 cd build
